71f25c07aea4283a66cd0945a6d87ef10cfd1985
david.seb.fischer <david.seb.fischer@gmail.com>
2020-09-07 23:20:20 +0200

    added missing rtfd files

---
+++ sfaira/__init__.py
@@ -0,0 +1,11 @@
+from ._version import get_versions
+
+__version__ = get_versions()['version']
+del get_versions
+
+__author__ = ', '.join([
+    'theislab'
+])
+__email__ = ', '.join([
+    'david.fischer@helmholtz-muenchen.de'
+])
