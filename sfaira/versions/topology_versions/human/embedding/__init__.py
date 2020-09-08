30e98c4b8d63ec7406cd53546da95666aa90e309
david.seb.fischer <david.seb.fischer@gmail.com>
2020-09-08 16:14:25 +0200

    fixed bug in human architecture dict

--- sfaira/versions/topology_versions/human/embedding/__init__.py
+++ sfaira/versions/topology_versions/human/embedding/__init__.py
@@ -1,6 +1,6 @@
 from sfaira.versions.topology_versions.human.embedding.ae import AE_TOPOLOGIES
 from sfaira.versions.topology_versions.human.embedding.linear import LINEAR_TOPOLOGIES
-from sfaira.versions.topology_versions.mouse.embedding.nmf import NMF_TOPOLOGIES
+from sfaira.versions.topology_versions.human.embedding.nmf import NMF_TOPOLOGIES
 from sfaira.versions.topology_versions.human.embedding.vae import VAE_TOPOLOGIES
 from sfaira.versions.topology_versions.human.embedding.vaeiaf import VAEIAF_TOPOLOGIES
 from sfaira.versions.topology_versions.human.embedding.vaevamp import VAEVAMP_TOPOLOGIES
