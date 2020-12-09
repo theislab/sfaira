6ad19e3312ddc1746c9750c1e68f6cfaa3b34ca0
le-ander <20015434+le-ander@users.noreply.github.com>
2020-12-09 23:28:12 +0100

    fix celltype versioning through sfaira_extension

--- sfaira/versions/celltype_versions/__init__.py
+++ sfaira/versions/celltype_versions/__init__.py
@@ -9,15 +9,38 @@ human = human.ORGAN_DICT
 # Load versions from extension if available:
 try:
     from sfaira_extension.versions.celltype_versions import SPECIES_DICT as SPECIES_DICT_EXTENSION
-    mouse_e = SPECIES_DICT_EXTENSION["mouse"]
-    human_e = SPECIES_DICT_EXTENSION["human"]
-    for k in mouse.keys():
-        if k in mouse_e.keys():
-            mouse[k].celltype_universe.update(mouse_e[k])
-            mouse[k].ontology.update(mouse_e[k])
-        if k in mouse_e.keys():
-            human[k].celltype_universe.update(human_e[k])
-            human[k].ontology.update(human_e[k])
+
+    for organ in mouse.keys():
+        if organ in SPECIES_DICT_EXTENSION["mouse"].keys():
+            for v in SPECIES_DICT_EXTENSION["mouse"][organ].versions:
+                if v in mouse[organ].celltype_universe.keys():
+                    mouse[organ].celltype_universe[v] = {
+                        **mouse[organ].celltype_universe[v],
+                        **SPECIES_DICT_EXTENSION["mouse"][organ].celltype_universe[v]
+                    }
+                    mouse[organ].ontology[v] = {
+                        **mouse[organ].ontology[v],
+                        **SPECIES_DICT_EXTENSION["mouse"][organ].ontology[v]
+                    }
+                else:
+                    mouse[organ].celltype_universe[v] = SPECIES_DICT_EXTENSION["mouse"][organ].celltype_universe[v]
+                    mouse[organ].ontology[v] = SPECIES_DICT_EXTENSION["mouse"][organ].ontology[v]
+
+    for organ in human.keys():
+        if organ in SPECIES_DICT_EXTENSION["human"].keys():
+            for v in SPECIES_DICT_EXTENSION["human"][organ].versions:
+                if v in human[organ].celltype_universe.keys():
+                    human[organ].celltype_universe[v] = {
+                        **human[organ].celltype_universe[v],
+                        **SPECIES_DICT_EXTENSION["human"][organ].celltype_universe[v]
+                    }
+                    human[organ].ontology[v] = {
+                        **human[organ].ontology[v],
+                        **SPECIES_DICT_EXTENSION["human"][organ].ontology[v]
+                    }
+                else:
+                    human[organ].celltype_universe[v] = SPECIES_DICT_EXTENSION["human"][organ].celltype_universe[v]
+                    human[organ].ontology[v] = SPECIES_DICT_EXTENSION["human"][organ].ontology[v]
 except ImportError:
     pass
 
