cdc2badeb5321d4243124ae03c019ff285011d41
le-ander <20015434+le-ander@users.noreply.github.com>
2020-12-15 22:15:08 +0100

    remove references to dataset specific adata_ids

--- sfaira/consts/__init__.py
+++ sfaira/consts/__init__.py
@@ -1,3 +1,3 @@
 from sfaira.consts.adata_fields import ADATA_IDS_BASE, ADATA_IDS_EXTENDED
-from sfaira.consts.adata_fields import ADATA_IDS_CELLXGENE
+from sfaira.consts.adata_fields import ADATA_IDS_CELLXGENE, ADATA_IDS_SFAIRA
 from sfaira.consts.meta_data_files import META_DATA_FIELDS
