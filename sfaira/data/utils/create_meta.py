8e82a9ed5fd734d00d192fecfba925725eb06358
david.seb.fischer <david.seb.fischer@gmail.com>
2021-01-05 14:02:07 +0100

    allowed multiple parallelisations in meta data loading

--- sfaira/data/utils/create_meta.py
+++ sfaira/data/utils/create_meta.py
@@ -11,23 +11,35 @@ path = str(sys.argv[1])
 path_meta = str(sys.argv[2])
 processes = int(str(sys.argv[2]))
 
+parallelise_across_studies = True
+
 ds = sfaira.data.dataloaders.DatasetSuperGroupSfaira(
     path=path, meta_path=path_meta, cache_path=path_meta
 )
+if parallelise_across_studies and processes > 1:
+    ds.load_all(
+        celltype_version=None,
+        annotated_only=False,
+        match_to_reference=None,
+        remove_gene_version=True,
+        load_raw=True,
+        allow_caching=False,
+        processes=processes,
+    )
 for dsg in list(ds.dataset_groups):
-    if processes > 1:
-        # Need explicit call to load_all to uses multiprocess, otherwise this would be automatically  evoced in meta data
-        # writing. Note: paralellisation is once per data set group (ie study, so that not all data is loaded into
-        # memory!
-        ds.load_all(
-            celltype_version=None,
-            annotated_only=False,
-            match_to_reference=None,
-            remove_gene_version=True,
-            load_raw=True,
-            allow_caching=False,
-            processes=processes,
-        )
+    if not parallelise_across_studies and processes > 1:
+            # Need explicit call to load_all to uses multiprocess, otherwise this would be automatically  evoced in meta data
+            # writing. Note: paralellisation is once per data set group (ie study, so that not all data is loaded into
+            # memory!
+            ds.load_all(
+                celltype_version=None,
+                annotated_only=False,
+                match_to_reference=None,
+                remove_gene_version=True,
+                load_raw=True,
+                allow_caching=False,
+                processes=processes,
+            )
     for k in dsg.ids:
         print(k)
         dsg.datasets[k].write_meta(dir_out=path_meta)
