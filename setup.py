b4962cd6099be2785d8fd2a7ba19529fa7dbd094
Zethson <lukas.heumos@posteo.net>
2021-01-14 10:48:32 +0100

    add setup.py classifiers & tf 2.x restriction
    Signed-off-by: Zethson <lukas.heumos@posteo.net>

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import versioneer
 
 author = 'theislab'
 author_email = 'david.fischer@helmholtz-muenchen.de'
-description = "sfaira is a model and a data repository for single-cell data in a single python package. "
+description = "sfaira is a model and a data repository for single-cell data in a single python package."
 
 with open("README.rst", "r") as fh:
     long_description = fh.read()
@@ -15,6 +15,15 @@ setup(
     description=description,
     long_description=long_description,
     long_description_content_type="text/markdown",
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
     packages=find_packages(),
     install_requires=[
         'anndata>=0.7',
@@ -23,7 +32,7 @@ setup(
         'pandas',
         'scipy>=1.2.1',
         'tqdm',
-        'tensorflow'  # TODO Remove and add to tensorflow profile
+        'tensorflow>=2.0.0'  # TODO Remove and add to tensorflow profile
     ],
     extras_require={
         'tensorflow': [
@@ -46,13 +55,6 @@ setup(
         ],
         'extension': [
             "sfaira_extension",
-        ],
-        'docs': [
-            'sphinx',
-            'sphinx-autodoc-typehints',
-            'sphinx_rtd_theme',
-            'jinja2',
-            'docutils',
         ],
     },
     version=versioneer.get_version(),
