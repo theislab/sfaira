ef14bfc842191f4e08320e9501d9d786933a00a2
david.seb.fischer <david.seb.fischer@gmail.com>
2021-01-19 12:09:39 +0100

    extended data loader documentation

--- sfaira/consts/adata_fields.py
+++ sfaira/consts/adata_fields.py
@@ -199,7 +199,7 @@ class ADATA_IDS_SFAIRA(ADATA_IDS_EXTENDED):
         self.organ_allowed_entries = None
         self.organism_allowed_entries = ["mouse", "human"]
         self.protocol_allowed_entries = None
-        self.sex_allowed_entries = None
+        self.sex_allowed_entries = ["female", "male"]
         self.subtissue_allowed_entries = None
         self.year_allowed_entries = list(range(2000, 3000))
         # Free fields that are not constrained:
