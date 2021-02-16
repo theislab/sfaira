c87edef4c7e28298bb9defd3d740869b617f841e
le-ander <20015434+le-ander@users.noreply.github.com>
2021-02-16 19:20:16 +0100

    read 10 PBMC dataset directly from raw downlaoded file

--- sfaira/data/dataloaders/loaders/dno_doi_10x_genomics/human_blood_2019_10x_10xGenomics_001.py
+++ sfaira/data/dataloaders/loaders/dno_doi_10x_genomics/human_blood_2019_10x_10xGenomics_001.py
@@ -1,23 +1,14 @@
-import anndata
 import os
 from typing import Union
+import scipy.sparse
+import anndata as ad
+import numpy as np
+import tables
 
 from sfaira.data import DatasetBase
 
 
 class Dataset(DatasetBase):
-    """
-    This data loader requires manual preprocessing of the raw datafile. To download the data, use the link in the
-    `.download_website` attribute of this class. To create the file required by this dataloader, run the following
-    python code:
-
-    import scanpy
-    scanpy.read_10x_h5("pbmc_10k_v3_filtered_feature_bc_matrix.h5").write("pbmc_10k_v3_filtered_feature_bc_matrix.h5ad")
-
-    :param data_path:
-    :param meta_path:
-    :param kwargs:
-    """
 
     def __init__(
             self,
@@ -47,5 +38,27 @@ class Dataset(DatasetBase):
         self.var_ensembl_col = "gene_ids"
 
     def _load(self):
-        fn = os.path.join(self.data_dir, "pbmc_10k_v3_filtered_feature_bc_matrix.h5ad")
-        self.adata = anndata.read(fn)
+        fn = os.path.join(self.data_dir, "pbmc_10k_v3_filtered_feature_bc_matrix.h5")
+        with tables.open_file(str(fn), 'r') as f:
+            dsets = {}
+            for node in f.walk_nodes('/matrix', 'Array'):
+                dsets[node.name] = node.read()
+            M, N = dsets['shape']
+            data = dsets['data']
+            if dsets['data'].dtype == np.dtype('int32'):
+                data = dsets['data'].view('float32')
+                data[:] = dsets['data']
+            matrix = scipy.sparse.csr_matrix(
+                (data, dsets['indices'], dsets['indptr']),
+                shape=(N, M),
+            )
+            self.adata = ad.AnnData(
+                matrix,
+                dict(obs_names=dsets['barcodes'].astype(str)),
+                dict(
+                    var_names=dsets['name'].astype(str),
+                    gene_ids=dsets['id'].astype(str),
+                    feature_types=dsets['feature_type'].astype(str),
+                    genome=dsets['genome'].astype(str),
+                ),
+            )
