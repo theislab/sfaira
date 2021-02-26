fb1970df01e2c60f19f7b8242d519f0572382832
david.seb.fischer <david.seb.fischer@gmail.com>
2021-02-26 19:33:02 +0100

    moved imports into load in yaml example

--- sfaira/data/dataloaders/loaders/d10_1016_j_cell_2018_08_067/human_laminapropriaofmucosaofcolon_2019_10xsequencing_kinchen_001.py
+++ sfaira/data/dataloaders/loaders/d10_1016_j_cell_2018_08_067/human_laminapropriaofmucosaofcolon_2019_10xsequencing_kinchen_001.py
@@ -1,15 +1,15 @@
-import os
-import pandas as pd
-import anndata as ad
-import scipy.sparse
-import numpy as np
-
 from sfaira.data import DatasetBaseGroupLoadingManyFiles
 
 
 class Dataset(DatasetBaseGroupLoadingManyFiles):
 
     def _load(self):
+        import os
+        import pandas as pd
+        import anndata as ad
+        import scipy.sparse
+        import numpy as np
+
         fn = [
             os.path.join(self.data_dir, f"GSE114374_Human_{self.sample_fn}_expression_matrix.txt.gz"),
             os.path.join(self.data_dir, f"{self.sample_fn.lower()}_meta_data_stromal_with_donor.txt"),
