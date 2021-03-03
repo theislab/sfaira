cc30be7b12f46bb6a72af479ff1ad8dad86ffea5
david.seb.fischer <david.seb.fischer@gmail.com>
2021-03-03 13:45:59 +0100

    moved d10_1038_s41586_020_2922_4 to yaml interface

--- sfaira/data/dataloaders/loaders/d10_1038_s41586_020_2922_4/human_lung_2020_x_travaglini_001.py
+++ sfaira/data/dataloaders/loaders/d10_1038_s41586_020_2922_4/human_lung_2020_x_travaglini_001.py
@@ -3,48 +3,6 @@ import os
 import scipy.sparse
 import numpy as np
 
-from sfaira.data import DatasetBase
-
-SAMPLE_FNS = [
-    "droplet_normal_lung_blood_scanpy.20200205.RC4.h5ad",
-    "facs_normal_lung_blood_scanpy.20200205.RC4.h5ad"
-]
-
-
-class Dataset(DatasetBase):
-    """
-    ToDo split by sample / patient in obs columns:
-      bio replicates droplet file "orig.ident"+"sample"+"magnetic.selection",
-      bio replicates facs file "patient"+"sample"
-      tech replicates droplet file "channel",
-      tech replicates facs file "plate.barcode"
-    """
-
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        synapse_id = {
-            "droplet_normal_lung_blood_scanpy.20200205.RC4.h5ad": "syn21625095",
-            "facs_normal_lung_blood_scanpy.20200205.RC4.h5ad": "syn21625142"
-        }
-
-        self.download_url_data = f"{synapse_id[self.sample_fn]},{self.sample_fn}"
-        self.download_url_meta = None
-
-        self.author = "Travaglini"
-        self.doi = "10.1038/s41586-020-2922-4"
-        self.healthy = True
-        self.normalization = "raw"
-        self.organ = "lung"
-        self.organism = "human"
-        self.protocol = "10X sequencing" if self.sample_fn.split("_")[0] == "droplet" else "Smart-seq2"
-        self.state_exact = "healthy"
-        self.year = 2020
-
-        self.obs_key_cellontology_original = "free_annotation"
-        self.var_symbol_col = "index"
-
-        self.set_dataset_id(idx=1)
-
 
 def load(data_dir, sample_fn, **kwargs):
     fn = os.path.join(data_dir, sample_fn)
