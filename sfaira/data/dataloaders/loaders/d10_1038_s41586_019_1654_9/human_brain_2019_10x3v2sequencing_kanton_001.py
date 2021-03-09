2a9fc735a75332d8cd4b51a51dab0fb116540212
le-ander <20015434+le-ander@users.noreply.github.com>
2021-03-09 18:30:29 +0100

    write kanton load function

--- sfaira/data/dataloaders/loaders/d10_1038_s41586_019_1654_9/human_brain_2019_10x3v2sequencing_kanton_001.py
+++ sfaira/data/dataloaders/loaders/d10_1038_s41586_019_1654_9/human_brain_2019_10x3v2sequencing_kanton_001.py
@@ -1,26 +1,24 @@
 import anndata
 import os
-import scipy.sparse
-import numpy as np
+import scipy.io
+import zipfile
+import pandas
 
 
 def load(data_dir, **kwargs):
-    fn = os.path.join(data_dir, "")
-
-    fn = "../data/d10_1038_s41586_019_1654_9/E-MTAB-7552.processed.3.zip"
-    with zipfile.ZipFile(fn) as archive:
+    fn = [
+        os.path.join(data_dir, "E-MTAB-7552.processed.3.zip"),
+        os.path.join(data_dir, "E-MTAB-7552.processed.1.zip"),
+        os.path.join(data_dir, "E-MTAB-7552.processed.7.zip")
+    ]
+    with zipfile.ZipFile(fn[0]) as archive:
         x = scipy.io.mmread(archive.open('human_cell_counts_GRCh38.mtx')).T.tocsr()
+    with zipfile.ZipFile(fn[1]) as archive:
+        var = pandas.read_csv(archive.open('genes_GRCh38.txt'), sep="\t", index_col=1, names=['ensembl', 'genetype'])
+    with zipfile.ZipFile(fn[2]) as archive:
+        obs = pandas.read_csv(archive.open('metadata_human_cells.tsv'), sep="\t", index_col=0)
+    adata = anndata.AnnData(X=x, var=var, obs=obs)
 
-    fn = "../data/d10_1038_s41586_019_1654_9/E-MTAB-7552.processed.1.zip"
-    with zipfile.ZipFile(fn) as archive:
-        var = pd.read_csv(archive.open('genes_GRCh38.txt'), sep="\t", index_col=1, names=['ensembl', 'genetype'])
-
-    fn = "../data/d10_1038_s41586_019_1654_9/E-MTAB-7552.processed.7.zip"
-    with zipfile.ZipFile(fn) as archive:
-        obs = pd.read_csv(archive.open('metadata_human_cells.tsv'), sep="\t", index_col=0)
-
-    adata = ad.AnnData(X=x, var=var, obs=obs)
-
-    # remove non-protein coding genes?
+    # TODO: remove non-protein coding genes?
 
     return adata
