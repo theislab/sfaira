09f372c3b1b8d6992d38d04dea6c99f102b53496
Lukas Heumos <lukas.heumos@posteo.net>
2021-03-11 16:42:26 +0100

    Moving the CLI to yaml based dataloaders (#182)
    * first draft of yaml creation
    
    Signed-off-by: Zethson <lukas.heumos@posteo.net>
    
    * add extra_description.txt to template
    
    Signed-off-by: Zethson <lukas.heumos@posteo.net>
    
    * fix template creation wf
    
    Signed-off-by: Zethson <lukas.heumos@posteo.net>
    
    * add new clean and lint
    
    Signed-off-by: Zethson <lukas.heumos@posteo.net>
    
    * add adding datasets documentation
    
    Signed-off-by: Zethson <lukas.heumos@posteo.net>
    
    * fix extra_description.txt content
    
    Signed-off-by: Zethson <lukas.heumos@posteo.net>
    
    * remove clean and test dataloader docs
    
    Signed-off-by: Zethson <lukas.heumos@posteo.net>
    
    * add multiple datasets template
    
    Signed-off-by: Zethson <lukas.heumos@posteo.net>
    
    * fix create template wf
    
    Signed-off-by: Zethson <lukas.heumos@posteo.net>
    
    * fix YAML structure
    
    Signed-off-by: Zethson <lukas.heumos@posteo.net>
    
    * add lint for multiple datasets
    
    Signed-off-by: Zethson <lukas.heumos@posteo.net>

--- sfaira/commands/clean_dataloader.py
+++ sfaira/commands/clean_dataloader.py
@@ -1,5 +1,7 @@
 import logging
-from subprocess import Popen
+
+import yaml
+from boltons.iterutils import remap
 
 log = logging.getLogger(__name__)
 
@@ -11,39 +13,12 @@ class DataloaderCleaner:
 
     def clean_dataloader(self) -> None:
         """
-        Removes any unwanted artifacts from a dataloader Python script and formats the code.
-        1. Any line that starts with # self.      <- outcommented attribute
-        2. Any line that starts with # SFARA:     <- explicitly marked full comments
-        3. Any line with             # something  <- comments after attributes
-        4. Runs black
+        Removes unused keys from the yaml file
         """
-        # Remove all unwanted artifacts
-        cleaned_content = []
+        with open(self.path) as yaml_file:
+            content = yaml.load(yaml_file, Loader=yaml.FullLoader)
+            drop_falsey = lambda path, key, value: bool(value)
+            clean = remap(content, visit=drop_falsey)
 
-        with open(self.path, 'r') as data_loader_file:
-            content = data_loader_file.readlines()
-            for line in content:
-                line_stripped = line.strip()
-                if line_stripped.startswith('# self.'):
-                    continue
-                elif line_stripped.startswith('# SFAIRA:'):
-                    continue
-                else:
-                    if '#' in line:
-                        if len(line.split('#')) > 1:
-                            try:
-                                cleaned_content += f'{line.split("#")[0]}\n'
-                            except KeyError:
-                                cleaned_content += line
-                    else:
-                        cleaned_content += line
-
-        with open(self.path, 'w') as data_loader_file:
-            for line in cleaned_content:
-                data_loader_file.write(line)
-            data_loader_file.write('\n')
-
-        # run black
-        black = Popen(['black', self.path],
-                      universal_newlines=True, shell=False, close_fds=True)
-        (black_stdout, black_stderr) = black.communicate()
+            with open(self.path, 'w') as file:
+                yaml.dump(clean, file)
