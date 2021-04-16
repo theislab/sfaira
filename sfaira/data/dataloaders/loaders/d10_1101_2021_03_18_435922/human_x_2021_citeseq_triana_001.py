785ee1a76a8d4dec1c55e0fdf303e286a479354f
david.seb.fischer <david.seb.fischer@gmail.com>
2021-04-16 12:05:01 +0200

    updated loading

--- sfaira/data/dataloaders/loaders/d10_1101_2021_03_18_435922/human_x_2021_citeseq_triana_001.py
+++ sfaira/data/dataloaders/loaders/d10_1101_2021_03_18_435922/human_x_2021_citeseq_triana_001.py
@@ -6,15 +6,17 @@ def load(data_dir, sample_fn, **kwargs):
     import anndata2ri
     from rpy2.robjects import r
 
-    fn = os.path.join(data_dir, "tissue.rdata")
+    file_name = sample_fn.split("-")[0]
+    object_name = sample_fn.split("-")[1]
+    fn = os.path.join(data_dir, file_name + ".rda")
     anndata2ri.activate()  # TODO: remove global activation of anndata2ri and use localconverter once it's fixed
+    # file_name is already a Seurat object, maybe not optimally solved here, need a way to select correct count matrix.
     adata = r(
         f"library(Seurat)\n"
         f"load('{fn}')\n"
-        f"new_obj = CreateSeuratObject(counts = tissue@raw.data)\n"
+        f"new_obj = CreateSeuratObject(counts = {object_name}@assays$RNA@counts)\n"
         f"new_obj@meta.data = tissue@meta.data\n"
         f"as.SingleCellExperiment(new_obj)\n"
     )
-    adata.obs["nGene"] = adata.obs["nGene"].astype(np.int32)
 
     return adata
