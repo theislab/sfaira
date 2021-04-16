f94f194c113470d2813e5700747931b8f546f2f6
david.seb.fischer <david.seb.fischer@gmail.com>
2021-04-16 11:15:41 +0200

    first commit

---
+++ sfaira/data/dataloaders/loaders/d10_1101_2021_03_18_435922/human_x_2021_citeseq_triana_001.py
@@ -0,0 +1,20 @@
+import numpy as np
+import os
+
+
+def load(data_dir, sample_fn, **kwargs):
+    import anndata2ri
+    from rpy2.robjects import r
+
+    fn = os.path.join(data_dir, "tissue.rdata")
+    anndata2ri.activate()  # TODO: remove global activation of anndata2ri and use localconverter once it's fixed
+    adata = r(
+        f"library(Seurat)\n"
+        f"load('{fn}')\n"
+        f"new_obj = CreateSeuratObject(counts = tissue@raw.data)\n"
+        f"new_obj@meta.data = tissue@meta.data\n"
+        f"as.SingleCellExperiment(new_obj)\n"
+    )
+    adata.obs["nGene"] = adata.obs["nGene"].astype(np.int32)
+
+    return adata
