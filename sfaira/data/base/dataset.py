7966aa88ccae40a933b2e9ddfc9a2c42f18fcd52
le-ander <20015434+le-ander@users.noreply.github.com>
2021-04-28 09:33:45 +0200

    reduce step size in matrix copying to get tid of "cannot convert integer scalar" scipy error

--- sfaira/data/base/dataset.py
+++ sfaira/data/base/dataset.py
@@ -626,7 +626,7 @@ class DatasetBase(abc.ABC):
         # copying this over to the new matrix in chunks of size `steps` prevents a strange scipy error:
         # ... scipy/sparse/compressed.py", line 922, in _zero_many i, j, offsets)
         # ValueError: could not convert integer scalar
-        step = 2000
+        step = 500
         if step < len(idx_feature_map):
             i = 0
             for i in range(0, len(idx_feature_map), step):
