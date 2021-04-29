stabilised size factor computation, fixed store unit tests and added dense conversion for feature indexing in store generator

sfaira/data/base/distributed_store.py
@@ -95,6 +95,7 @@ class DistributedStore:
             batch_size: int = 1,
             obs_keys: List[str] = [],
             continuous_batches: bool = True,
+            return_dense: bool = True,
     ) -> iter:
         """
         Yields an unbiased generator over observations in the contained data sets.
@@ -104,6 +105,8 @@ class DistributedStore:
             in self.adatas.
         :param continuous_batches: Whether to build batches of batch_size across data set boundaries if end of one
             data set is reached.
+        :param return_dense: Whether to return count data .X as dense batches. This allows more efficient feature
+            indexing if the store is sparse (column indexing on csr matrices is slow).
         :return: Generator function which yields batch_size at every invocation.
             The generator returns a tuple of (.X, .obs) with types:
 
@@ -116,7 +119,7 @@ class DistributedStore:
         # Use feature space sub-selection based on assembly if provided, will use full feature space otherwise.
         if self.genome_container is not None:
             var_names_target = self.genome_container.ensembl
-            var_idx = np.array([var_names_store.index(x) for x in var_names_target])
+            var_idx = np.sort([var_names_store.index(x) for x in var_names_target])
             # Check if index vector is just full ordered list of indices, in this case, subsetting is unnecessary.
             if len(var_idx) == len(var_names_store) and np.all(var_idx == np.arange(0, len(var_names_store))):
                 var_idx = None
@@ -132,24 +135,28 @@ class DistributedStore:
                 if continuous_batches and x_last is not None:
                     # Prepend data set with residual data from last data set.
                     remainder_start = x_last.shape[0]
-                    n_obs = v.n_obs + remainder_start
                 else:
                     # Partition into equally sized batches up to last batch.
                     remainder_start = 0
-                    n_obs = v.n_obs
-                remainder = n_obs % batch_size
-                batch_starts = [
-                    np.min([0, int(x * batch_size - remainder_start)])
-                    for x in np.arange(1, n_obs // batch_size + int(remainder > 0))
+                remainder = (v.n_obs + remainder_start) % batch_size
+                batch_starts_ends = [
+                    (
+                        np.max([0, int(x * batch_size - remainder_start)]),
+                        np.max([0, int(x * batch_size - remainder_start)]) + batch_size
+                    )
+                    for x in np.arange(0, (v.n_obs + remainder_start) // batch_size + int(remainder > 0))
                 ]
-                n_batches = len(batch_starts)
+                n_batches = len(batch_starts_ends)
                 # Iterate over batches:
-                for j, z in enumerate(batch_starts):
-                    batch_end = int(z + batch_size)
-                    x = v.X[z:batch_end, :]
+                for j, (s, e) in enumerate(batch_starts_ends):
+                    x = v.X[s:e, :]
+                    # Do dense conversion now so that col-wise indexing is not slow, often, dense conversion
+                    # would be done later anyway.
+                    if return_dense:
+                        x = x.todense()
                     if var_idx is not None:
                         x = x[:, var_idx]
-                    obs = v.obs[obs_keys].iloc[z:batch_end, :]
+                    obs = v.obs[obs_keys].iloc[s:e, :]
                     assert isinstance(obs, pd.DataFrame), f"{type(obs)}"
                     if continuous_batches and remainder > 0 and i < (n_datasets - 1) and j == (n_batches - 1):
                         # Cache incomplete last batch to append to next first batch of next data set.
@@ -164,6 +171,9 @@ class DistributedStore:
                         else:
                             assert False, type(x)
                         obs = pd.concat(objs=[obs_last, obs], axis=0)
+                        # Reset over-hang cache:
+                        x_last = None
+                        obs_last = None
                         yield x, obs
                     else:
                         # Yield current batch.

sfaira/estimators/keras.py
@@ -24,13 +24,16 @@ from .metrics import custom_mse, custom_negll_nb, custom_negll_gaussian, custom_
 
 
 def prepare_sf(x):
+    """
+    Uses a minimal size factor of 1e-3 for total counts / 1e4
+    """
     if len(x.shape) == 2:
         sf = np.asarray(x.sum(axis=1)).flatten()
     elif len(x.shape) == 1:
         sf = np.asarray(x.sum()).flatten()
     else:
         raise ValueError("x.shape > 2")
-    sf = np.log(sf / 1e4 + 1e-10)
+    sf = np.log(np.maximum(sf / 1e4, 1e-3))
     return sf
 
 
@@ -549,6 +552,7 @@ class EstimatorKerasEmbedding(EstimatorKeras):
             self,
             generator_helper,
             idx: Union[np.ndarray, None],
+            store_generator_batch_size: int = 1,
     ):
         """
         Yield a basic generator based on which a tf dataset can be built.
@@ -567,7 +571,7 @@ class EstimatorKerasEmbedding(EstimatorKeras):
         # Prepare data reading according to whether anndata is backed or not:
         if self.using_store:
             generator_raw = self.data.generator(
-                batch_size=1,
+                batch_size=store_generator_batch_size,
                 obs_keys=[],
                 continuous_batches=True,
             )
@@ -579,8 +583,8 @@ class EstimatorKerasEmbedding(EstimatorKeras):
                     if counter in idx:
                         x_sample = z[0]
                         if isinstance(x_sample, scipy.sparse.csr_matrix):
-                            x_sample = np.asarray(x_sample.todense())
-                        x_sample = x_sample.flatten()
+                            x_sample = x_sample.todense()
+                        x_sample = np.asarray(x_sample).flatten()
                         yield generator_helper(x_sample=x_sample)
 
             n_features = self.data.n_vars
@@ -672,8 +676,8 @@ class EstimatorKerasEmbedding(EstimatorKeras):
                         if counter in idx:
                             x_sample = z[0]
                             if isinstance(x_sample, scipy.sparse.csr_matrix):
-                                x_sample = np.asarray(x_sample.todense())
-                            x_sample = x_sample.flatten()
+                                x_sample = x_sample.todense()
+                            x_sample = np.asarray(x_sample).flatten()
                             sf_sample = prepare_sf(x=x_sample)[0]
                             y_sample = z[1]["cell_ontology_class"].values[0]
                             yield (x_sample, sf_sample), (x_sample, cell_to_class[y_sample])
@@ -778,7 +782,7 @@ class EstimatorKerasEmbedding(EstimatorKeras):
         else:
             return {}
 
-    def evaluate(self, batch_size: int = 1, max_steps: int = np.inf):
+    def evaluate(self, batch_size: int = 64, max_steps: int = np.inf):
         """
         Evaluate the custom model on test data.
 
@@ -790,7 +794,7 @@ class EstimatorKerasEmbedding(EstimatorKeras):
         """
         return self.evaluate_any(idx=self.idx_test, batch_size=batch_size, max_steps=max_steps)
 
-    def predict(self):
+    def predict(self, batch_size: int = 64, max_steps: int = np.inf):
         """
         return the prediction of the model
 
@@ -800,14 +804,14 @@ class EstimatorKerasEmbedding(EstimatorKeras):
         if self.idx_test is None or self.idx_test.any():  # true if the array is not empty or if the passed value is None
             dataset = self._get_dataset(
                 idx=self.idx_test,
-                batch_size=64,
+                batch_size=batch_size,
                 mode='predict'
             )
             return self.model.predict_reconstructed(x=dataset)
         else:
             return np.array([])
 
-    def predict_embedding(self):
+    def predict_embedding(self, batch_size: int = 64, max_steps: int = np.inf):
         """
         return the prediction in the latent space (z_mean for variational models)
 
@@ -817,14 +821,14 @@ class EstimatorKerasEmbedding(EstimatorKeras):
         if self.idx_test is None or self.idx_test.any():  # true if the array is not empty or if the passed value is None
             dataset = self._get_dataset(
                 idx=self.idx_test,
-                batch_size=64,
+                batch_size=batch_size,
                 mode='predict'
             )
             return self.model.predict_embedding(x=dataset, variational=False)
         else:
             return np.array([])
 
-    def predict_embedding_variational(self):
+    def predict_embedding_variational(self, batch_size: int = 64, max_steps: int = np.inf):
         """
         return the prediction of z, z_mean, z_log_var in the variational latent space
 
@@ -834,7 +838,7 @@ class EstimatorKerasEmbedding(EstimatorKeras):
         if self.idx_test is None or self.idx_test:  # true if the array is not empty or if the passed value is None
             dataset = self._get_dataset(
                 idx=self.idx_test,
-                batch_size=64,
+                batch_size=batch_size,
                 mode='predict'
             )
             return self.model.predict_embedding(x=dataset, variational=True)
@@ -1057,6 +1061,7 @@ class EstimatorKerasCelltype(EstimatorKeras):
             generator_helper,
             idx: Union[np.ndarray, None],
             weighted: bool = False,
+            store_generator_batch_size: int = 1,
     ):
         """
         Yield a basic generator based on which a tf dataset can be built.
@@ -1079,7 +1084,7 @@ class EstimatorKerasCelltype(EstimatorKeras):
             if weighted:
                 raise ValueError("using weights with store is not supported yet")
             generator_raw = self.data.generator(
-                batch_size=1,
+                batch_size=store_generator_batch_size,
                 obs_keys=["cell_ontology_class"],
                 continuous_batches=True,
             )
@@ -1092,8 +1097,8 @@ class EstimatorKerasCelltype(EstimatorKeras):
                     if counter in idx:
                         x_sample = z[0]
                         if isinstance(x_sample, scipy.sparse.csr_matrix):
-                            x_sample = np.asarray(x_sample.todense())
-                        x_sample = x_sample.flatten()
+                            x_sample = x_sample.todense()
+                        x_sample = np.asarray(x_sample).flatten()
                         y_sample = onehot_encoder(z[1]["cell_ontology_class"].values[0])
                         yield generator_helper(x_sample, y_sample, 1.)
 
@@ -1183,11 +1188,7 @@ class EstimatorKerasCelltype(EstimatorKeras):
             CustomTprClasswise(k=self.ntypes)
         ]
 
-    def predict(
-            self,
-            batch_size: int = 1,
-            max_steps: int = np.inf,
-    ):
+    def predict(self, batch_size: int = 64, max_steps: int = np.inf):
         """
         Return the prediction of the model
 
@@ -1207,7 +1208,7 @@ class EstimatorKerasCelltype(EstimatorKeras):
         else:
             return np.array([])
 
-    def ytrue(self):
+    def ytrue(self, batch_size: int = 64, max_steps: int = np.inf):
         """
         Return the true labels of the test set.
 
@@ -1216,7 +1217,7 @@ class EstimatorKerasCelltype(EstimatorKeras):
         if self.idx_test is None or self.idx_test.any():   # true if the array is not empty or if the passed value is None
             x, y, w = self._get_dataset(
                 idx=self.idx_test,
-                batch_size=None,
+                batch_size=batch_size,
                 mode='eval'
             )
             return y
@@ -1255,7 +1256,7 @@ class EstimatorKerasCelltype(EstimatorKeras):
         else:
             return {}
 
-    def evaluate(self, batch_size: int = 1, max_steps: int = np.inf, weighted: bool = False):
+    def evaluate(self, batch_size: int = 64, max_steps: int = np.inf, weighted: bool = False):
         """
         Evaluate the custom model on local data.
 

sfaira/unit_tests/data/test_store.py
@@ -1,6 +1,7 @@
 import numpy as np
 import os
 import pytest
+import time
 from typing import List
 
 from sfaira.data import DistributedStore
@@ -61,30 +62,47 @@ def test_type_targets():
 
 @pytest.mark.parametrize("batch_size", [1, 10])
 @pytest.mark.parametrize("obs_keys", [[], ["cell_ontology_class"]])
-@pytest.mark.parametrize("continuous_batches", [True])
-@pytest.mark.parametrize("assembly", [None, MOUSE_GENOME_ANNOTATION])
-@pytest.mark.parametrize("subset", [{}, {"biotype": "protein_coding"}])
-def test_generator_shapes(batch_size: int, obs_keys: List[str], continuous_batches: bool, assembly: str, subset: dict):
+@pytest.mark.parametrize("continuous_batches", [True, False])
+@pytest.mark.parametrize("gc", [(None, {}), (MOUSE_GENOME_ANNOTATION, {"biotype": "protein_coding"})])
+def test_generator_shapes(batch_size: int, obs_keys: List[str], continuous_batches: bool, gc: tuple):
     """
     Test generators queries do not throw errors and that output shapes are correct.
     """
+    assembly, subset = gc
+    store_path = cached_store_writing(dir_data=dir_data, dir_meta=dir_meta, assembly=MOUSE_GENOME_ANNOTATION)
+    store = DistributedStore(cache_path=store_path)
     if assembly is not None:
         gc = GenomeContainer(assembly=assembly)
         gc.subset(**subset)
-    else:
-        gc = None
-    store_path = cached_store_writing(dir_data=dir_data, dir_meta=dir_meta, assembly=MOUSE_GENOME_ANNOTATION)
-    store = DistributedStore(cache_path=store_path)
-    store.genome_container = gc
+        store.genome_container = gc
     g = store.generator(
         batch_size=batch_size,
         obs_keys=obs_keys,
         continuous_batches=continuous_batches,
     )
-    x, obs = next(g)
+    batch_sizes = []
+    t0 = time.time()
+    for i, z in enumerate(g()):
+        x_i, obs_i = z
+        assert x_i.shape[0] == obs_i.shape[0]
+        if i == 0:  # First batch hast correct shape, last batch not necessarily!
+            x = x_i
+            obs = obs_i
+        batch_sizes.append(x_i.shape[0])
+    tdelta = time.time() - t0
+    print(f"time for iterating over generator:"
+          f" {tdelta} for {np.sum(batch_sizes)} cells in {len(batch_sizes)} batches,"
+          f" {tdelta / len(batch_sizes)} per batch.")
+    if continuous_batches:
+        # Only the last batch can be of different size:
+        assert np.sum(batch_sizes != batch_size) <= 1
+    else:
+        # Only the last batch in each data set can be of different size:
+        assert np.sum(batch_sizes != batch_size) <= len(store.adatas.keys())
     assert x.shape[0] == batch_size, (x.shape, batch_size)
     assert obs.shape[0] == batch_size, (obs.shape, batch_size)
     assert x.shape[1] == store.n_vars, (x.shape, store.n_vars)
     assert obs.shape[1] == len(obs_keys), (x.shape, obs_keys)
+    assert np.sum(batch_sizes) == store.n_obs, (x.shape, obs_keys)
     if assembly is not None:
         assert x.shape[1] == gc.n_var, (x.shape, gc.n_var)