6a349784c4c758eab06b002c80c408c4604d54c1
david.seb.fischer <david.seb.fischer@gmail.com>
2021-04-30 21:25:02 +0200

    fixed mouse topologies

--- sfaira/versions/topologies/mouse/celltype/celltypemlp.py
+++ sfaira/versions/topologies/mouse/celltype/celltypemlp.py
@@ -2,7 +2,7 @@ CELLTYPEMLP_TOPOLOGIES = {
     "0.0.1": {
         "model_type": "mlp",
         "input": {
-            "genome": "Homo_sapiens.GRCh38.102",
+            "genome": "Mus_musculus.GRCm38.102",
             "genes": ["biotype", "protein_coding"],
         },
         "output": {
