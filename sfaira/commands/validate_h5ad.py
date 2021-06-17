8b54b892ff595fd63dae04637481b1caaa843cd7
david.seb.fischer <david.seb.fischer@gmail.com>
2021-06-17 16:38:19 +0200

    fixed cli

--- sfaira/commands/validate_h5ad.py
+++ sfaira/commands/validate_h5ad.py
@@ -10,7 +10,7 @@ log = logging.getLogger(__name__)
 class H5adValidator:
 
     def __init__(self, test_h5ad, schema=None):
-        self.fn: str = test_h5ad
+        self.fn_h5ad: str = test_h5ad
         if schema not in ["cellxgene"]:
             raise ValueError(f"Did not recognize schema {schema}")
         self.schema = schema
@@ -19,14 +19,14 @@ class H5adValidator:
     @property
     def adata(self):
         if self.adata is None:
-            self._adata = anndata.read_h5ad(filename=self.fn)
+            self._adata = anndata.read_h5ad(filename=self.fn_h5ad)
         return self._adata
 
     def test_schema(self) -> None:
         """Verify that object elements match schema definitions."""
         if self.schema == "cellxgene":
-            from cellxgene.backend.server.converters.schema.validate import validate_adata
-            validate_adata(adata=self.adata, shallow=False)
+            from cellxgene_schema import validate
+            validate.validate(h5ad_path=self.fn_h5ad, shallow=False)
         else:
             assert False
 
@@ -37,5 +37,4 @@ class H5adValidator:
         else:
             x = np.unique(np.asarray(self.adata.X))
         deviation_from_integer = np.minimum(x % 1, 1. - x % 1)
-        print(f"Maximum deviation from integer found: {np.max(deviation_from_integer)}")
-        assert np.max(deviation_from_integer) < 1e-3
+        assert np.max(deviation_from_integer) < 1e-6
