4d924f42b2e13ee8ea25bba4fbf03c4f91256002
david.seb.fischer <david.seb.fischer@gmail.com>
2021-07-26 14:28:15 +0200

    fixed imports

--- sfaira/data/__init__.py
+++ sfaira/data/__init__.py
@@ -1,5 +1,7 @@
-from sfaira.data.base import DatasetBase, DatasetGroup, DatasetGroupDirectoryOriented, \
-    DatasetSuperGroup, load_store, DistributedStoreBase, DistributedStoreH5ad, DistributedStoreDao
+from sfaira.data.dataloaders.base import DatasetBase, DatasetGroup, DatasetGroupDirectoryOriented, \
+    DatasetSuperGroup
+from sfaira.data.store import load_store, DistributedStoreMultipleFeatureSpaceBase, DistributedStoresH5ad, \
+    DistributedStoresDao
 from . import dataloaders
 from .dataloaders import Universe
 from .interactive import DatasetInteractive
