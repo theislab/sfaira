66877107683ff189867b6718a9c3f4ec3b265c69
david.seb.fischer <david.seb.fischer@gmail.com>
2021-08-11 13:58:35 +0200

    fixed generator in test script

--- sfaira/data/utils_scripts/test_store.py
+++ sfaira/data/utils_scripts/test_store.py
@@ -59,7 +59,8 @@ def time_gen(_store, store_format, kwargs) -> List[float]:
         gc.subset(symbols=["VTA1", "MLXIPL", "BAZ1B", "RANBP9", "PPARGC1A", "DDX25", "CRYAB"])
         _store.genome_container = gc
     del kwargs["var_subset"]
-    _gen, _ = _store.generator(**kwargs)()
+    _gen, _ = _store.generator(**kwargs)
+    _gen = _gen()
     _measurements = []
     for _ in range(N_DRAWS):
         _t0 = time.time()
