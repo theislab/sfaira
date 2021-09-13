c21555f66490cd78c20e134104be2e868402ad9e
Laura Martens <laura.d.martens@icloud.com>
2021-09-13 15:11:40 +0200

    addressed comments

--- sfaira/data/dataloaders/loaders/d10_1038_s41590_021_00933_1/human_blood_2021_citeseq_nathan_001.py
+++ sfaira/data/dataloaders/loaders/d10_1038_s41590_021_00933_1/human_blood_2021_citeseq_nathan_001.py
@@ -10,11 +10,12 @@ def load(data_dir, sample_fn, **kwargs):
     fn = os.path.join(data_dir, sample_fn)
     adata = anndata.read_text(fn, delimiter="\t")
     adata = adata.T
-    adata.X = scipy.sparse.csc_matrix(adata.X)
+    adata.X = scipy.sparse.csr_matrix(adata.X)
     metadata = pd.read_csv(os.path.join(data_dir, "GSE158769_meta_data.txt.gz"), sep="\t")
-    adata.obs = adata.obs.join(metadata.set_index('cell_id'))
-    adata = adata[~adata.obs.cluster_name.isna()]
+    adata.obs = adata.obs.join(metadata.set_index("cell_id"))
+    adata.obs.loc[adata.obs.cluster_name.isna(), "cluster_name"] = "unknown"
 
     adata.obs["disease"] = adata.obs["TB_status"].map(disease_map)
 
+    adata.obsm["X_umap"] = adata.obs.loc[:, ["UMAP_1", "UMAP_2"]].values
     return adata
