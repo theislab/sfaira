6d0eb83f2aa6d5d8be0d76400d7c2d875d028875
Leander <20015434+le-ander@users.noreply.github.com>
2021-09-28 08:32:16 +0200

    Fix UI (#378)
    * add unit tests for UI
    
    * fix ui
    
    * store sfaira_repo_url in sfaira.consts
    
    * remove capitalisation of gene symbols when streamlining features
    
    * extend unit test to check that gene symbols match the genome container after feature streamlining
    
    * added check that genome assemblies are the same between celltype and embedding zoo
    
    * making sure var_names are automatically set according to schema in ui
    
    * keep original obs_names when loading a dataset through the ui

--- sfaira/unit_tests/tests_by_submodule/ui/test_userinterface.py
+++ sfaira/unit_tests/tests_by_submodule/ui/test_userinterface.py
@@ -1,9 +1,13 @@
 import numpy as np
 import os
 from typing import Union
+import pandas as pd
+import urllib.request
 
 from sfaira.ui import UserInterface
+from sfaira.unit_tests.data_for_tests.loaders.utils import prepare_dsg
 from sfaira.unit_tests import DIR_TEMP
+from sfaira.consts import SFAIRA_REPO_URL
 
 
 class HelperUi:
@@ -17,30 +21,62 @@ class HelperUi:
     basic_estimator_test(). See _test_call() for an example.
     """
 
-    def simulate(self):
-        """
-        Simulate basic data example used for unit test.
+    def __init__(self):
+        self.temp_fn = os.path.join(DIR_TEMP, "test_data")
 
-        Sets attribute .data with simulated data.
+    def prepare_local_tempfiles(self):
+        # create temp directory
+        if not os.path.exists(self.temp_fn):
+            os.makedirs(self.temp_fn)
+        # download an example weight from sfaira repo
+        lookuptable = pd.read_csv(os.path.join(SFAIRA_REPO_URL, 'model_lookuptable.csv'), header=0, index_col=0)
+        url = lookuptable.loc[0, "model_file_path"]
+        if os.path.basename(url) not in os.listdir(self.temp_fn):
+            urllib.request.urlretrieve(url, os.path.join(self.temp_fn, os.path.basename(url)))
+
+    def _get_adata(self):
+        """
+        Create an adata object for use in unit tests
 
         :return:
         """
-        pass
+        dsg = prepare_dsg(rewrite=True, load=False)
+        dsg.subset(key="id", values=["human_lung_2021_None_mock4_001_no_doi_mock4"])
+        dsg.load()
+        return dsg.adata
 
-    def test_basic(self):
+    def test_local_repo_ui_init(self):
         """
-        Test all relevant model methods.
-
+        Test if the sfaira UI can be sucessfully initialised using a local model repository
 
         :return:
         """
-        temp_fn = os.path.join(DIR_TEMP, "test_data")
-        self.ui = UserInterface(custom_repo=temp_fn, sfaira_repo=False)
+        self.ui = UserInterface(custom_repo=self.temp_fn, sfaira_repo=False)
+
+    def test_public_repo_ui_init(self):
+        """
+        Test if the sfaira UI can be sucessfully initialised using the public sfaira model repository
+
+        :return:
+        """
+        self.ui = UserInterface(custom_repo=None, sfaira_repo=True)
+
+    def test_data_and_model_loading(self):
+        self.ui = UserInterface(custom_repo=None, sfaira_repo=True)
+        self.ui.zoo_embedding.model_id = 'embedding_human-blood-ae-0.2-0.1_theislab'
+        self.ui.zoo_celltype.model_id = 'celltype_human-blood-mlp-0.1.3-0.1_theislab'
+        test_data = self._get_adata()
+        self.ui.load_data(test_data, gene_ens_col='index')
+        self.ui.load_model_celltype()
+        self.ui.load_model_embedding()
+        self.ui.predict_all()
+        assert "X_sfaira" in self.ui.data.adata.obsm_keys()
+        assert "celltypes_sfaira" in self.ui.data.adata.obs_keys()
 
 
-def _test_for_fatal():
-    """
-    TODO need to simulate/add look up table as part of unit tests locally
-    """
+def test_ui():
     ui = HelperUi()
-    ui.test_basic()
+    ui.prepare_local_tempfiles()
+    ui.test_public_repo_ui_init()
+    ui.test_local_repo_ui_init()
+    ui.test_data_and_model_loading()
