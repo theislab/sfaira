7ccb879504fca4a4670c55527361e57f1d1e497b
davidsebfischer <david.seb.fischer@gmail.com>
2021-10-18 11:55:12 +0200

    updated CL throughout

--- sfaira/unit_tests/update_loaders/update_celltypes.py
+++ sfaira/unit_tests/update_loaders/update_celltypes.py
@@ -1,39 +1,39 @@
 import numpy as np
 import os
+import pandas as pd
 import pydoc
 import pytest
 
 from sfaira.data.dataloaders.loaders import DatasetSuperGroupLoaders
-from sfaira.data import Universe
 
-from sfaira.unit_tests.directories import DIR_SFAIRA_LOADERS
+from sfaira.unit_tests.directories import DIR_DATA_LOADERS_CACHE
+
+SYMBOL_COL = "target"
+ID_COL = "target_id"
+UNKNOWN_IDS = ["UNKNOWN", "NOT_A_CELL"]
 
 
 def test_celltype_update():
     """
     Warning: changes files in package if ontology mismatches are found!
     """
-    # Directory choice hyperparamters:
-    dir_prefix = "d"
-    dir_exclude = []
-    dssg = DatasetSuperGroupLoaders()
+    dssg = DatasetSuperGroupLoaders(data_path=DIR_DATA_LOADERS_CACHE, meta_path=DIR_DATA_LOADERS_CACHE,
+                                    cache_path=DIR_DATA_LOADERS_CACHE)
     for dsg in dssg.dataset_groups:
         collection = dsg.collection_id
-        for k, ds in dsg.datasets.items():
-            fn_map = str(pydoc.locate(f"sfaira.data.dataloaders.loaders.{collection}.{k}.tsv"))
-            if os.path.exists(fn_map):
-                # Access reading and value protection mechanisms from first data set loaded in group.
-                ds.read_ontology_class_map(fn=fn_map)
-                ds.write_ontology_class_map(fn=fn_map, protected_writing=False)
-            else:
-                print(f"did not find map for {collection}, {k}: {fn_map}")
-
-#for d in os.listdir(DIR_SFAIRA_LOADERS):
-#    if os.path.isdir(os.path.join(DIR_SFAIRA_LOADERS, d)):  # Narrow down to directories.
-#        if d[:len(dir_prefix)] == dir_prefix and d not in dir_exclude:  # Narrow down to data set directories.
-#            path_dsg = str(pydoc.locate(f"sfaira.data.dataloaders.loaders.{f}.FILE_PATH"))
-#for f in os.listdir(d):  # Loop over files in target directory.
-#    if os.path.isfile(os.path.join(d, f)):  # Narrow down to files.
-#        if f.split(".")[-1] == "tsv":  # Narrow down to ontology map files.
-#            tab = pd.
-
+        for ds in dsg.datasets.values():
+            path_dsg = os.path.dirname(str(pydoc.locate(f"sfaira.data.dataloaders.loaders.{collection}.FILE_PATH")))
+            for f in os.listdir(path_dsg):
+                if f.split(".")[-1] == "tsv":  # Narrow down to ontology map files.
+                    fn_map = os.path.join(path_dsg, f)
+                    if os.path.exists(fn_map):
+                        # Access reading and value protection mechanisms from first data set loaded in group.
+                        tab = ds._read_ontology_class_map(fn=fn_map)
+                        tab[SYMBOL_COL] = [
+                            ds.celltypes_universe.onto_cl.convert_to_name(x)
+                            if x not in UNKNOWN_IDS else x
+                            for x in tab[ID_COL].values
+                        ]
+                        tab.to_csv(path_or_buf=fn_map, index=False, sep="\t")
+                    else:
+                        print(f"did not find map for {collection}, {ds.fn_ontology_class_map_tsv}: {fn_map}")
