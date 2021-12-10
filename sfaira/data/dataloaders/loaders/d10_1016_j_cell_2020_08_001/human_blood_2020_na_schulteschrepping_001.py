317722644cfc5dac6c20ba240a6ec736912f6e1c
davidsebfischer <david.seb.fischer@gmail.com>
2021-12-10 15:54:19 +0100

    finished loader setup

--- sfaira/data/dataloaders/loaders/d10_1016_j_cell_2020_08_001/human_blood_2020_na_schulteschrepping_001.py
+++ sfaira/data/dataloaders/loaders/d10_1016_j_cell_2020_08_001/human_blood_2020_na_schulteschrepping_001.py
@@ -6,6 +6,7 @@ import scipy.sparse
 def load(data_dir, sample_fn, **kwargs):
     fn = os.path.join(data_dir, sample_fn)
     adata = anndata.read(fn)
-    adata.X = scipy.sparse.csc_matrix(adata.X)
+    adata.X = scipy.sparse.csr_matrix(adata.X)
+    adata.obs["age"] = [f"{x}-year-old human stage" for x in adata.obs["age"].values]
 
     return adata
