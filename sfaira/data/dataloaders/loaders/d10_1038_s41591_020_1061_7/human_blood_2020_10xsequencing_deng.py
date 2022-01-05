0d5e7db662298a4f8ce23ffa9eb835b0b0dbd548
Laura Martens <laura.d.martens@icloud.com>
2022-01-05 17:36:12 +0100

    Add init

--- sfaira/data/dataloaders/loaders/d10_1038_s41591_020_1061_7/human_blood_2020_10xsequencing_deng.py
+++ sfaira/data/dataloaders/loaders/d10_1038_s41591_020_1061_7/human_blood_2020_10xsequencing_deng.py
@@ -1,6 +1,5 @@
 
 import anndata
-import gzip
 import os
 import pandas as pd
 import scipy.io
@@ -30,9 +29,10 @@ def load(data_dir, sample_fn, **kwargs):
     metadata = pd.read_excel(fn[1], skiprows=2, skipfooter=3)
 
     adata.obs['Sample ID'] = name
-    adata.obs = adata.obs.merge(metadata, on='Sample ID')
+    adata.obs = adata.obs.reset_index().merge(metadata, on='Sample ID').set_index("index")
     adata.obs["Age"] = [f"{x}-year-old human stage" for x in adata.obs["Age"].values]
     adata.obs["Gender"] = adata.obs["Gender"].map(sex_map)
     adata.obs["Histology"] = adata.obs["Histology"].map(disease_map)
 
+    adata.obs = adata.obs.astype(str)
     return adata
