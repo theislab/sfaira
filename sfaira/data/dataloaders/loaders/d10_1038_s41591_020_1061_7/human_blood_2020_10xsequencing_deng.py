d33f6ee749534fb81f3bc90347f242ff438621c2
Laura Martens <laura.d.martens@icloud.com>
2022-01-05 16:53:09 +0100

    Create dataloader

---
+++ sfaira/data/dataloaders/loaders/d10_1038_s41591_020_1061_7/human_blood_2020_10xsequencing_deng.py
@@ -0,0 +1,38 @@
+
+import anndata
+import gzip
+import os
+import pandas as pd
+import scipy.io
+import tarfile
+
+sex_map = {"M": "male", "F": "female"}
+disease_map = {"DLBCL": "diffuse large B-cell lymphoma",
+               "tFL": "follicular lymphoma",
+               "PMBCL": "primary mediastinal large B-cell lymphoma"}
+
+
+def load(data_dir, sample_fn, **kwargs):
+    fn = [os.path.join(data_dir, "GSE151511_RAW.tar"),
+          os.path.join(data_dir, "41591_2020_1061_MOESM3_ESM.xlsx")]
+
+    with tarfile.open(fn[0]) as tar:
+        with tarfile.open(fileobj=tar.extractfile(sample_fn)) as sample:
+            name = sample.getnames()[0]
+
+            x = scipy.io.mmread(sample.extractfile(f'{name}/{name}_matrix.mtx')).T.tocsr()
+            obs = pd.read_csv(sample.extractfile(f'{name}/{name}_barcodes.tsv'), header=None, sep="\t", index_col=0)
+            obs.index.name = None
+            var = pd.read_csv(sample.extractfile(f'{name}/{name}_genes.tsv'), header=None, sep="\t")
+            var.columns = ["ensembl", "names"]
+            var.index = var["ensembl"].values
+            adata = anndata.AnnData(X=x, obs=obs, var=var)
+    metadata = pd.read_excel(fn[1], skiprows=2, skipfooter=3)
+
+    adata.obs['Sample ID'] = name
+    adata.obs = adata.obs.merge(metadata, on='Sample ID')
+    adata.obs["Age"] = [f"{x}-year-old human stage" for x in adata.obs["Age"].values]
+    adata.obs["Gender"] = adata.obs["Gender"].map(sex_map)
+    adata.obs["Histology"] = adata.obs["Histology"].map(disease_map)
+
+    return adata
