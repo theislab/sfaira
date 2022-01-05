70812a620140d363859fab831c34b219cb6f51f0
Laura Martens <laura.d.martens@icloud.com>
2022-01-05 12:21:11 +0100

    Fix linting

--- sfaira/data/dataloaders/loaders/d10_1038_s41467_020_15543_y/human_blood_2020_10xsequencing_cano_gamez.py
+++ sfaira/data/dataloaders/loaders/d10_1038_s41467_020_15543_y/human_blood_2020_10xsequencing_cano_gamez.py
@@ -13,8 +13,7 @@ class Dataset(DatasetBase):
         self.download_url_data = [
             'https://storage.googleapis.com/otar040-cytoimmunogenomics/publication-project-data/scRNAseq/NCOMMS-19-7936188_scRNAseq_raw_UMIs.mtx',
             'https://storage.googleapis.com/otar040-cytoimmunogenomics/publication-project-data/scRNAseq/NCOMMS-19-7936188_scRNAseq_genes.tsv',
-            'https://storage.googleapis.com/otar040-cytoimmunogenomics/publication-project-data/scRNAseq/NCOMMS-19-7936188_scRNAseq_barcodes.tsv'
-            ] 
+            'https://storage.googleapis.com/otar040-cytoimmunogenomics/publication-project-data/scRNAseq/NCOMMS-19-7936188_scRNAseq_barcodes.tsv']
         self.download_url_meta = 'https://storage.googleapis.com/otar040-cytoimmunogenomics/publication-project-data/scRNAseq/NCOMMS-19-7936188_metadata.txt'
 
         self.assay_sc = "10x 3' v2"
@@ -33,25 +32,22 @@ class Dataset(DatasetBase):
 
         self.set_dataset_id(idx=1)
 
+
 def load(data_dir, **kwargs):
-        fn = [os.path.join(data_dir, "NCOMMS-19-7936188_scRNAseq_raw_UMIs.mtx"),
-              os.path.join(data_dir, 'NCOMMS-19-7936188_scRNAseq_barcodes.tsv'),
-              os.path.join(data_dir, 'NCOMMS-19-7936188_scRNAseq_genes.tsv'),
-              os.path.join(data_dir, "NCOMMS-19-7936188_metadata.txt")]
+    fn = [os.path.join(data_dir, "NCOMMS-19-7936188_scRNAseq_raw_UMIs.mtx"),
+          os.path.join(data_dir, 'NCOMMS-19-7936188_scRNAseq_barcodes.tsv'),
+          os.path.join(data_dir, 'NCOMMS-19-7936188_scRNAseq_genes.tsv'),
+          os.path.join(data_dir, "NCOMMS-19-7936188_metadata.txt")]
 
-        with open(fn[0], 'rb') as mm:
-            X = scipy.io.mmread(mm).T.tocsr()
-        obs = pd.read_csv(fn[1],
-                          header=None, sep="\t", index_col=0)
-        obs.index.name = None
-        var = pd.read_csv(fn[2],
-                          header=None, sep="\t", names=['names'])
-        var.index = var['names'].values
+    with open(fn[0], 'rb') as mm:
+        X = scipy.io.mmread(mm).T.tocsr()
+    obs = pd.read_csv(fn[1], header=None, sep="\t", index_col=0)
+    obs.index.name = None
+    var = pd.read_csv(fn[2], header=None, sep="\t", names=['names'])
+    var.index = var['names'].values
 
-        adata = anndata.AnnData(X=X, obs=obs, var=var)
+    adata = anndata.AnnData(X=X, obs=obs, var=var)
 
-        metadata = pd.read_csv(fn[3], sep="\t")
-        adata.obs = adata.obs.join(metadata, how="inner")
-        return adata
-
-
+    metadata = pd.read_csv(fn[3], sep="\t")
+    adata.obs = adata.obs.join(metadata, how="inner")
+    return adata
