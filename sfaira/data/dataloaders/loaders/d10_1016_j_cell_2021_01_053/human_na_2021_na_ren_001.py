804c2f1998510ed68b28616d14e4a21bf59c3ac7
Laura Martens <laura.d.martens@icloud.com>
2022-01-05 14:34:56 +0100

    Fix linting

--- sfaira/data/dataloaders/loaders/d10_1016_j_cell_2021_01_053/human_na_2021_na_ren_001.py
+++ sfaira/data/dataloaders/loaders/d10_1016_j_cell_2021_01_053/human_na_2021_na_ren_001.py
@@ -37,33 +37,27 @@ sex_map = {
 
 
 def load(data_dir, **kwargs):
-    fn = [
-        os.path.join(data_dir, "GSE158055_covid19_counts.mtx.gz"),
-        os.path.join(data_dir, "GSE158055_covid19_barcodes.tsv.gz"),
-        os.path.join(data_dir, "GSE158055_covid19_features.tsv.gz"),
-        os.path.join(data_dir, "GSE158055_cell_annotation.csv.gz"),
-        os.path.join(data_dir, "GSE158055_sample_metadata.xlsx"),
-        ]
+    fn = [os.path.join(data_dir, "GSE158055_covid19_counts.mtx.gz"),
+          os.path.join(data_dir, "GSE158055_covid19_barcodes.tsv.gz"),
+          os.path.join(data_dir, "GSE158055_covid19_features.tsv.gz"),
+          os.path.join(data_dir, "GSE158055_cell_annotation.csv.gz"),
+          os.path.join(data_dir, "GSE158055_sample_metadata.xlsx")]
 
     with gzip.open(fn[0], 'rb') as mm:
         X = scipy.io.mmread(mm).T.tocsr()
-        
-    obs = pd.read_csv(fn[1],
-                    header=None, sep="\t", index_col=0)
+    obs = pd.read_csv(fn[1], header=None, sep="\t", index_col=0)
     obs.index.name = None
-    var = pd.read_csv(fn[2],
-                    header=None, sep="\t", names=['names'])
+    var = pd.read_csv(fn[2], header=None, sep="\t", names=['names'])
     var.index = var['names'].values
 
     adata = anndata.AnnData(X=X, obs=obs, var=var)
 
     annotation = pd.read_csv(fn[3])
     metadata = pd.read_excel(fn[4], skiprows=20, skipfooter=37)
-    metadata.columns = metadata.columns.str.split('characteristics: ' ).str[-1]
-    tmp = annotation.merge(metadata, left_on = 'sampleID', right_on = 'Sample name').set_index('cellName')
+    metadata.columns = metadata.columns.str.split('characteristics: ').str[-1]
+    tmp = annotation.merge(metadata, left_on='sampleID', right_on='Sample name').set_index('cellName')
 
     adata.obs = adata.obs.join(tmp).astype(str)
-
 
     adata.obs["assay_sc"] = adata.obs["Single cell sequencing platform"].map(assay_sc_map)
     adata.obs["disease"] = adata.obs["SARS-CoV-2"].map(disease_map)
