bd30ac778bc97a6f369f265c42fcb048776bc612
davidsebfischer <david.seb.fischer@gmail.com>
2022-01-11 14:42:46 +0100

    extended store docs

--- sfaira/data/store/__init__.py
+++ sfaira/data/store/__init__.py
@@ -1,3 +1,5 @@
 from sfaira.data.store.io.load_store import load_store
-from sfaira.data.store.stores import DistributedStoreSingleFeatureSpace, DistributedStoreMultipleFeatureSpaceBase, \
+from sfaira.data.store.stores import DistributedStoreSingleFeatureSpace, \
+    DistributedStoreAnndata, DistributedStoreDao, \
+    DistributedStoreMultipleFeatureSpaceBase, \
     DistributedStoresAnndata, DistributedStoresDao, DistributedStoresH5ad
