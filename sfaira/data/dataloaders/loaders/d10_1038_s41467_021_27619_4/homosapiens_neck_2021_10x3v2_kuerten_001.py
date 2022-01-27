731d49b299a3025e7c92744e132c0a9a356c9456
Felix Fischer <fe.fischer@icloud.com>
2022-01-27 10:16:51 +0100

    put column 3 back into var dataframe and named column feature_class

--- sfaira/data/dataloaders/loaders/d10_1038_s41467_021_27619_4/homosapiens_neck_2021_10x3v2_kuerten_001.py
+++ sfaira/data/dataloaders/loaders/d10_1038_s41467_021_27619_4/homosapiens_neck_2021_10x3v2_kuerten_001.py
@@ -14,10 +14,8 @@ def load(data_dir, sample_fn, **kwargs):
         obs = pd.read_csv(tar.extractfile(f"{sample_fn}_barcodes.tsv.gz"), compression="gzip", header=None,
                           sep="\t", index_col=0)
         obs.index.name = None
-        var = pd.read_csv(
-            tar.extractfile(f"{sample_fn}_features.tsv.gz"), compression="gzip", header=None, sep="\t", usecols=[0, 1]
-        )
-        var.columns = ["ensembl", "symbol"]
+        var = pd.read_csv(tar.extractfile(f"{sample_fn}_features.tsv.gz"), compression="gzip", header=None, sep="\t")
+        var.columns = ["ensembl", "symbol", "feature_class"]
         var.index = var["ensembl"].values
         adata = anndata.AnnData(X=x, obs=obs, var=var)
     # Annotate organism in .var
