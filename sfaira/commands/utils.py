dbec66938cff22686130aa9fa0ba31e22ce2d21b
davidsebfischer <david.seb.fischer@gmail.com>
2022-02-07 15:47:04 +0100

    fixed lint

--- sfaira/commands/utils.py
+++ sfaira/commands/utils.py
@@ -11,4 +11,4 @@ def doi_lint(x: str) -> bool:
         - a sfaira formated DOI starting with "d10*" or "dno_doi*"
     """
     return re.match(r'\b10\.\d+/[\w.]+\b', x) or x.startswith("no_doi") or \
-           x.startswith("d10_") or x.startswith("dno_doi")
+        x.startswith("d10_") or x.startswith("dno_doi")
