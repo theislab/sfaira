f198b4baad6d52e9b75f39b69aad70e195b1330b
zethson <lukas.heumos@posteo.net>
2022-02-11 15:15:35 +0100

    add url check
    Signed-off-by: zethson <lukas.heumos@posteo.net>

---
+++ scripts/check_urls.py
@@ -0,0 +1,18 @@
+import itertools
+import urllib
+
+import sfaira
+
+u = sfaira.data.Universe()
+urls = []
+for x in u.datasets.values():
+    a = x.download_url_data
+    if a is not None:
+        urls.append(a) if isinstance(a, str) else urls.extend(a)
+    b = x.download_url_meta
+    if b is not None:
+        urls.append(b) if isinstance(b, str) else urls.extend(b)
+
+flat_urls = list(filter(lambda url: url is not None, list(itertools.chain(*urls))))
+for url in flat_urls:
+    assert urllib.request.urlopen(url).getcode() == 200
