ccb83b05bb9c922ba0503d84c859fb7bef821c2b
le-ander <20015434+le-ander@users.noreply.github.com>
2022-02-11 19:05:44 +0100

    lifting to newest schema and fixing .py loader

--- sfaira/data/dataloaders/loaders/d10_1101_2021_03_18_435922/human_bonemarrow_2021_citeseq_triana_001.py
+++ sfaira/data/dataloaders/loaders/d10_1101_2021_03_18_435922/human_bonemarrow_2021_citeseq_triana_001.py
@@ -1,4 +1,3 @@
-import numpy as np
 import os
 
 
@@ -6,17 +5,17 @@ def load(data_dir, sample_fn, **kwargs):
     import anndata2ri
     from rpy2.robjects import r
 
-    file_name = sample_fn.split("-")[0]
-    object_name = sample_fn.split("-")[1]
-    fn = os.path.join(data_dir, file_name + ".rda")
+    object_name = "Integration_200AB" if sample_fn == "200AB" else sample_fn
+    fn = os.path.join(data_dir, sample_fn + ".rda")
     anndata2ri.activate()  # TODO: remove global activation of anndata2ri and use localconverter once it's fixed
     # file_name is already a Seurat object, maybe not optimally solved here, need a way to select correct count matrix.
     adata = r(
         f"library(Seurat)\n"
         f"load('{fn}')\n"
         f"new_obj = CreateSeuratObject(counts = {object_name}@assays$RNA@counts)\n"
-        f"new_obj@meta.data = tissue@meta.data\n"
+        f"new_obj@meta.data = {object_name}@meta.data\n"
         f"as.SingleCellExperiment(new_obj)\n"
     )
-
+    if "class" not in adata.obs_keys():
+        adata.obs["class"] = "healthy"
     return adata
