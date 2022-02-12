ac48c799a4e062ceacc73cec515b9da4c49e8c4a
le-ander <20015434+le-ander@users.noreply.github.com>
2022-02-12 09:04:30 +0000

    [from sfaira cli] add dataloader d10_1101_2021_03_18_435922

---
+++ data/dataloaders/loaders/d10_1101_2021_03_18_435922/human_bonemarrow_2021_citeseq_triana_001.py
@@ -0,0 +1,22 @@
+import numpy as np
+import os
+
+
+def load(data_dir, sample_fn, **kwargs):
+    import anndata2ri
+    from rpy2.robjects import r
+
+    object_name = "Integration_200AB" if sample_fn == "200AB" else sample_fn
+    fn = os.path.join(data_dir, sample_fn + ".rda")
+    anndata2ri.activate()  # TODO: remove global activation of anndata2ri and use localconverter once it's fixed
+    # file_name is already a Seurat object, maybe not optimally solved here, need a way to select correct count matrix.
+    adata = r(
+        f"library(Seurat)\n"
+        f"load('{fn}')\n"
+        f"new_obj = CreateSeuratObject(counts = {object_name}@assays$RNA@counts)\n"
+        f"new_obj@meta.data = {object_name}@meta.data\n"
+        f"as.SingleCellExperiment(new_obj)\n"
+    )
+    if "class" not in adata.obs_keys():
+        adata.obs["class"] = "healthy"
+    return adata
