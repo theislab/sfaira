6c524a9988b4c5d311f29a9bb39bb8178e0a8ae1
dlhlisa <dlhlisa@126.com>
2022-02-15 19:26:53 +0100

    try dataloader

---
+++ sfaira/data/dataloaders/loaders/d10_1038_s41591_019_0750_6/lung_x_2020_x_laughney_001.py
@@ -0,0 +1,11 @@
+import anndata
+import os
+import scipy.sparse
+
+
+def load(data_dir, sample_fn, **kwargs):
+    fn = os.path.join(data_dir, sample_fn)
+    adata = anndata.read(fn)
+    adata.X = scipy.sparse.csr_matrix(adata.X)
+
+    return adata
