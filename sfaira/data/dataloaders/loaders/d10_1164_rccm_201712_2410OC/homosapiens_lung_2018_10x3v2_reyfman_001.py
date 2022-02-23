c4d59c7d7b355620b3cc1e42ae30aa8cd56af6a1
Felix Fischer <fe.fischer@icloud.com>
2022-02-23 15:59:28 +0100

    switched to scanpy to load data

--- sfaira/data/dataloaders/loaders/d10_1164_rccm_201712_2410OC/homosapiens_lung_2018_10x3v2_reyfman_001.py
+++ sfaira/data/dataloaders/loaders/d10_1164_rccm_201712_2410OC/homosapiens_lung_2018_10x3v2_reyfman_001.py
@@ -1,25 +1,20 @@
-import tarfile
-
-import anndata
 import os
+import shutil
+import tarfile
+import tempfile
 
-import pandas as pd
-import scipy.sparse
-import h5py
+import scanpy as sc
 
 
 def load(data_dir, sample_fn, **kwargs):
     fn = os.path.join(data_dir, 'GSE122960_RAW.tar')
+
     with tarfile.open(fn) as tar:
-        f = h5py.File(tar.extractfile(f'{sample_fn}_filtered_gene_bc_matrices_h5.h5'), 'r')['GRCh38']
-        x = scipy.sparse.csc_matrix((f['data'], f['indices'], f['indptr']), shape=f['shape']).T
-        var = pd.DataFrame({'feature_id': f['genes'], 'feature_symbol': f['gene_names']}).assign(
-            feature_id=lambda xx: xx.feature_id.str.decode('utf-8'),
-            feature_symbol=lambda xx: xx.feature_symbol.str.decode('utf-8')
-        )
-        obs = pd.DataFrame({'barcode': f['barcodes']}).assign(
-            barcode=lambda xx: xx.barcode.str.decode('utf-8')
-        )
-        adata = anndata.AnnData(X=x, obs=obs, var=var)
+        with tempfile.TemporaryDirectory() as tmpdir:
+            f = tar.extractfile(f'{sample_fn}_filtered_gene_bc_matrices_h5.h5')
+            tmppth = tmpdir + "/decompressed.h5"
+            with open(tmppth, "wb") as output_f:
+                shutil.copyfileobj(f, output_f)
+            adata = sc.read_10x_h5(tmppth)
 
     return adata
