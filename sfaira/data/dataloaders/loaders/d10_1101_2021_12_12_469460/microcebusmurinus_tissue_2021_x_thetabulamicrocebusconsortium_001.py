3730b6e2fdde47a00b6ead34375f123458b4fc25
ICB Karin Hrovatin <karin.hrovatin@icb-lisa.scidom.de>
2022-03-16 15:41:06 +0100

    loader

--- sfaira/data/dataloaders/loaders/d10_1101_2021_12_12_469460/microcebusmurinus_tissue_2021_x_thetabulamicrocebusconsortium_001.py
+++ sfaira/data/dataloaders/loaders/d10_1101_2021_12_12_469460/microcebusmurinus_tissue_2021_x_thetabulamicrocebusconsortium_001.py
@@ -1,22 +1,11 @@
-# +
 import anndata
 import os
-
-from tempfile import TemporaryDirectory
-from zipfile import ZipFile
-
-
-# -
 
 
 # the data_dir argument will be automatically set by sfaira to the folder where your datafiles lie
 def load(data_dir,**kwargs):
     sample_fn="31777475"
-    adata=anndata.read(data_dir+os.sep+sample_fn,backed='r')
+    adata=anndata.read(data_dir+os.sep+sample_fn)
     return adata  # your load function needs to return an AnnData object
 
-# + active=""
-#
-# -
 
-#
