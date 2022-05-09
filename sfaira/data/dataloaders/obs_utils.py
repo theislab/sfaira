81a7eeaad1abf103ad24bf8a6ab84c7c59101f94
David S. Fischer <david.seb.fischer@gmail.com>
2022-05-09 14:09:28 +0200

    fixed access to dataset ID in obs name cleaning (#657)

--- sfaira/data/dataloaders/obs_utils.py
+++ sfaira/data/dataloaders/obs_utils.py
@@ -233,7 +233,7 @@ def streamline_obs_uns(adata: anndata.AnnData,
         if not keep_symbol_obs and col_name in adata.obs.columns:
             del adata.obs[col_name]
     if clean_obs_names:
-        adata.obs.index = [f"{id}_{i}" for i in range(1, adata.n_obs + 1)]
+        adata.obs.index = [f"{dataset_id}_{i}" for i in range(1, adata.n_obs + 1)]
 
     # Make sure that correct unknown_metadata_identifier is used in .uns, .obs and .var metadata
     unknown_old = adata_input_ids.unknown_metadata_identifier
