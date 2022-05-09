2fc5e3b7d92521c62da5341bc9fbd0bbbe840073
Felix Fischer <fe.fischer@icloud.com>
2022-05-09 11:36:10 +0200

    replaced dicts with corresponding tsv files for d10_1101_753806 dataloader

--- sfaira/data/dataloaders/loaders/d10_1101_753806/homosapiens_lungparenchyma_2020_10xsequencing_habermann_001.py
+++ sfaira/data/dataloaders/loaders/d10_1101_753806/homosapiens_lungparenchyma_2020_10xsequencing_habermann_001.py
@@ -16,10 +16,8 @@ def load(data_dir, **kwargs):
     adata.obs = pd.read_csv(fn[2], index_col=0, header=None, names=["barcodes"])
     obs = pd.read_csv(fn[3], index_col=0)
     obs2 = pd.read_csv(fn[4], index_col=0)
-    obs["Chemistry"] = [{"3_prime_V2": "10x 3' v2", "5_prime": "10x 5' v1"}[obs2.loc[x, "Chemistry"]]
-                        for x in obs["orig.ident"].values]
-    obs["Gender"] = [{"F": "female", "M": "male", "Unknown": "unknown"}[obs2.loc[x, "Gender"]]
-                     for x in obs["orig.ident"].values]
+    obs["Chemistry"] = [obs2.loc[x, "Chemistry"] for x in obs["orig.ident"].values]
+    obs["Gender"] = [obs2.loc[x, "Gender"] for x in obs["orig.ident"].values]
     adata = adata[obs.index.tolist(), :].copy()
     adata.obs = obs
 
