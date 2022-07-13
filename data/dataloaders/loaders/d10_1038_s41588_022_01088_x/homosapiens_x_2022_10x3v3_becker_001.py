405a29fc677522ec6d7a02646bef0d4fb3769b44
Soroor Hediyeh-zadeh <soroorh@student.unimelb.edu.au>
2022-07-13 15:52:24 +0200

    10.1038/s41588-022-01088-x (#678)
    * homosapiens_None_2022_10x3v3_beckerwinston_001_d10_1038_s41588_022_01088_x

---
+++ data/dataloaders/loaders/d10_1038_s41588_022_01088_x/homosapiens_x_2022_10x3v3_becker_001.py
@@ -0,0 +1,13 @@
+import anndata
+import os
+# import any packages you require for dataloading here. you can assume packages like numpy and pandas being available
+
+
+# the data_dir argument will be automatically set by sfaira to the folder where your datafiles lie
+def load(data_dir, **kwargs):
+    # replace my-data-file.h5ad with the filename you're loading
+    fn = os.path.join(data_dir, "becker_crc.h5ad")
+    # replace the simple data loading code below with the code required to load your data file(s)
+    adata = anndata.read(fn)
+
+    return adata  # your load function needs to return an AnnData object
