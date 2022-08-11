7c9cd6357274ace9f12efb44504a0e427fb26bb6
davidsebfischer <david.seb.fischer@gmail.com>
2022-08-11 11:17:53 +0200

    added data loader template

---
+++ sfaira/data/dataloaders/loaders/d10_1016_j_cell_2021_11_033/homosapiens_lung_2021_10x3v3_wendisch_001.py
@@ -0,0 +1,13 @@
+import anndata
+import os
+# import any packages you require for dataloading here. you can assume packages like numpy and pandas being available
+
+
+# the data_dir argument will be automatically set by sfaira to the folder where your datafiles lie
+# the sample_fn argument will be automatically set by sfaira to each of the sample_fns provided in the yaml top section
+def load(data_dir, sample_fn, **kwargs):
+    fn = os.path.join(data_dir, sample_fn)
+    # replace the simple data loading code below with the code required to load your data file(s)
+    adata = anndata.read(fn)
+
+    return adata  # your load function needs to return an AnnData object
