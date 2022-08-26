089895b5c6843e0fd119a6f64c9d8560d6488df1
davidsebfischer <david.seb.fischer@gmail.com>
2022-08-26 11:30:02 +0200

    loader updates

--- sfaira/data/dataloaders/loaders/d10_1126_sciimmunol_abd1554/human_blood_2020_10xsequencing_lee_001.py
+++ sfaira/data/dataloaders/loaders/d10_1126_sciimmunol_abd1554/human_blood_2020_10xsequencing_lee_001.py
@@ -27,7 +27,7 @@ sample_map = {"Sample1": "nCoV 1 scRNA-seq",
 
 
 def load(data_dir, sample_fn, **kwargs):
-    adata = sc.read_10x_mtx(data_dir, prefix="GSE149689_")
+    adata = sc.read_10x_mtx(data_dir, prefix="GSE149689_", var_names='gene_ids')
     adata.obs["sample"] = "Sample" + adata.obs.index.str.split("-").str[1]
     adata.obs["GEO upload info"] = adata.obs["sample"].map(sample_map)
 
