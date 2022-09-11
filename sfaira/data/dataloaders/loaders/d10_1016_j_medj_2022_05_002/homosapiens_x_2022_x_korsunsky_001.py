0101c2f851232e01c5b14cdec382f51311ec7ba0
david.seb.fischer <david.seb.fischer@gmail.com>
2022-09-11 15:48:47 +0200

    updated index

--- sfaira/data/dataloaders/loaders/d10_1016_j_medj_2022_05_002/homosapiens_x_2022_x_korsunsky_001.py
+++ sfaira/data/dataloaders/loaders/d10_1016_j_medj_2022_05_002/homosapiens_x_2022_x_korsunsky_001.py
@@ -14,6 +14,7 @@ def load(data_dir, **kwargs):
     x = scipy.io.mmread(fn_x).T.tocsr()
     tab_obs = pd.read_csv(fn_obs, index_col=0, header=None, sep="\t", dtype=str)
     tab_var = pd.read_csv(fn_var, index_col=0, header=None, sep="\t", dtype=str)
+    tab_var.index = [str(x) for x in tab_var.index]
     tab_meta = pd.read_csv(fn_meta, index_col=0, header=0, skiprows=[1], sep="\t", dtype=str)
     tab_meta["cell_type_within_tissue"] = [str(x) for x in tab_meta["cell_type_within_tissue"].values]
     adata = anndata.AnnData(x, obs=tab_obs, var=tab_var)
