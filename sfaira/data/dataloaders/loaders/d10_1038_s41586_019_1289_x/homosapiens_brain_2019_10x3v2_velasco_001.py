05f804e7a4dbd8a59950c34cd064428c0c92cad3
ICB Artur Szalata <artur.szalata@helmholtz-muenchen.de>
2023-01-19 11:46:13 +0100

    adding dataloader

---
+++ sfaira/data/dataloaders/loaders/d10_1038_s41586_019_1289_x/homosapiens_brain_2019_10x3v2_velasco_001.py
@@ -0,0 +1,30 @@
+import anndata as ad
+import os
+import pandas as pd
+import numpy as np
+import scipy
+
+
+def load(data_dir, sample_fn, **kwargs):
+    fn = os.path.join(data_dir, sample_fn)
+    fn_meta = os.path.join(data_dir, "meta_combined.txt")
+    
+    expr_df = pd.read_csv(fn, sep="\t", compression='gzip', index_col=0)
+    expr_df = expr_df.apply(np.expm1)
+    expr_df = expr_df.T
+    
+    metadata_df = pd.read_csv(fn_meta, sep="\t", index_col=0, header=0)
+    metadata_df = metadata_df.tail(-1)
+    
+    # multiply by the UMIs, divide by 10^6
+    arr = scipy.sparse.csr_matrix(
+        np.multiply(expr_df.values, metadata_df.nUMI.loc[
+            expr_df.index.tolist()].values.reshape(-1,1).astype(int)))
+    arr /= 10**6 
+    
+    adata = ad.AnnData(
+        X=arr, obs=metadata_df.loc[
+            expr_df.index.tolist()].copy(),
+        var=pd.DataFrame(index=expr_df.columns.values))
+
+    return adata
