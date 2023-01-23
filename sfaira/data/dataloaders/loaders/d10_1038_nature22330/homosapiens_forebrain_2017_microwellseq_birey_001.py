3bb676d50e12709a7b3fac942b036646cf796459
ICB Irena Sliskovic <irena.sliskovic@hpc-submit01.scidom.de>
2023-01-23 11:02:56 +0100

    update after first review

--- sfaira/data/dataloaders/loaders/d10_1038_nature22330/homosapiens_forebrain_2017_microwellseq_birey_001.py
+++ sfaira/data/dataloaders/loaders/d10_1038_nature22330/homosapiens_forebrain_2017_microwellseq_birey_001.py
@@ -4,14 +4,9 @@ import tarfile
 import anndata as ad
 import scipy.sparse
 import os
-# import any packages you require for dataloading here. you can assume packages like numpy and pandas being available
 
-
-# the data_dir argument will be automatically set by sfaira to the folder where your datafiles lie
 def load(data_dir, **kwargs):
-    # replace my-data-file.h5ad with the filename you're loading
     fn = os.path.join(data_dir, "GSE93811_RAW.tar")
-    # replace the simple data loading code below with the code required to load your data file(s)
     dfs = []
     obs = []
     with tarfile.open(fn) as tar:
@@ -21,5 +16,6 @@ def load(data_dir, **kwargs):
                 obs.append(member.name.split("_")[1])
             dfs.append(d)
     adata = ad.AnnData(X=scipy.sparse.csr_matrix(np.vstack((dfs[0], dfs[1]))), obs=pd.DataFrame({"sample": obs}), var=pd.DataFrame(index=dfs[0].columns.values))
+    adata.obs['organoid_age_days'] = "105"
+    adata.obs['cell_line'] = np.repeat('2242-1', len(adata.obs))    
     return adata
-# your load function needs to return an AnnData object
