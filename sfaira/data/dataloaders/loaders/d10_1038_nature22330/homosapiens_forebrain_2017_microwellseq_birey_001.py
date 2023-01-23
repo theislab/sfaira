8f0df73bb16fa75cb3219eb6193b9de7ceecaf00
ICB Irena Sliskovic <irena.sliskovic@hpc-submit01.scidom.de>
2023-01-23 11:14:19 +0100

    fix flake8

--- sfaira/data/dataloaders/loaders/d10_1038_nature22330/homosapiens_forebrain_2017_microwellseq_birey_001.py
+++ sfaira/data/dataloaders/loaders/d10_1038_nature22330/homosapiens_forebrain_2017_microwellseq_birey_001.py
@@ -5,6 +5,7 @@ import anndata as ad
 import scipy.sparse
 import os
 
+
 def load(data_dir, **kwargs):
     fn = os.path.join(data_dir, "GSE93811_RAW.tar")
     dfs = []
@@ -17,5 +18,5 @@ def load(data_dir, **kwargs):
             dfs.append(d)
     adata = ad.AnnData(X=scipy.sparse.csr_matrix(np.vstack((dfs[0], dfs[1]))), obs=pd.DataFrame({"sample": obs}), var=pd.DataFrame(index=dfs[0].columns.values))
     adata.obs['organoid_age_days'] = "105"
-    adata.obs['cell_line'] = np.repeat('2242-1', len(adata.obs))    
+    adata.obs['cell_line'] = np.repeat('2242-1', len(adata.obs))
     return adata
