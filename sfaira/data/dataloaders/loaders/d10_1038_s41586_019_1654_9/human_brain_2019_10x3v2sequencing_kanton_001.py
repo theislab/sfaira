a00f9ec9ad291a2d77af0699296faffc8c01a329
le-ander <20015434+le-ander@users.noreply.github.com>
2023-01-25 19:03:57 +0100

    update kanton dataloader

--- sfaira/data/dataloaders/loaders/d10_1038_s41586_019_1654_9/human_brain_2019_10x3v2sequencing_kanton_001.py
+++ sfaira/data/dataloaders/loaders/d10_1038_s41586_019_1654_9/human_brain_2019_10x3v2sequencing_kanton_001.py
@@ -1,34 +1,25 @@
 import anndata
 import os
 import scipy.io
-import zipfile
 import pandas
+import numpy as np
 
 
 def load(data_dir, **kwargs):
-
-    cell_line_dict = {
-        '409b2': '409B2',
-        'H9': 'WA09',
-        'Wibj2': 'HPSI0214i-wibj_2',
-        'Sc102a1': 'SC102A-1',
-        'Kucg2': 'HPSI0214i-kucg_2',
-        'Hoik1': 'HPSI0314i-hoik_1',
-        'Sojd3': 'HPSI0314i-sojd_3',
+    day_dict = {
+        "Organoid-2M": "60",
+        "Organoid-4M": "120",
+        "Organoid-1M": "30",
+        "iPSCs": "0",
+        "EB": "4",
+        "Neuroectoderm": "10",
+        "Neuroepithelium": "15",
     }
-
-    fn = [
-        os.path.join(data_dir, "E-MTAB-7552.processed.3.zip"),
-        os.path.join(data_dir, "E-MTAB-7552.processed.1.zip"),
-        os.path.join(data_dir, "E-MTAB-7552.processed.7.zip")
-    ]
-    with zipfile.ZipFile(fn[0]) as archive:
-        x = scipy.io.mmread(archive.open('human_cell_counts_GRCh38.mtx')).T.tocsr()
-    with zipfile.ZipFile(fn[1]) as archive:
-        var = pandas.read_csv(archive.open('genes_GRCh38.txt'), sep="\t", index_col=1, names=['ensembl', 'genetype'])
-    with zipfile.ZipFile(fn[2]) as archive:
-        obs = pandas.read_csv(archive.open('metadata_human_cells.tsv'), sep="\t", index_col=0)
+    x = scipy.io.mmread(os.path.join(data_dir, 'human_cell_counts_GRCh38.mtx')).T.tocsr().astype(np.float32)
+    var = pandas.read_csv(os.path.join(data_dir, 'genes_GRCh38.txt'), sep="\t", index_col=1, names=['ensembl', 'genetype'])
+    obs = pandas.read_csv(os.path.join(data_dir, 'metadata_human_cells.tsv'), sep="\t", index_col=0)
+    obs["celltype"] = obs['cl_FullLineage'].fillna(obs['cl_LineComp']).fillna("unknown")
+    obs["organoid_age_days"] = [day_dict[i] for i in obs["Stage"]]
     adata = anndata.AnnData(X=x, var=var, obs=obs)
-    adata.obs["Line"] = [cell_line_dict[x] for x in adata.obs["Line"]]
-
+    adata.var_names_make_unique()
     return adata
