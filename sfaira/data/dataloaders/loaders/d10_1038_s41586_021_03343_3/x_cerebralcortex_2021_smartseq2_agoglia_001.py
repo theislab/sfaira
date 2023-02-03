0e25d3d58daa1b7334e3a7b5b3c7485c567b6905
le-ander <20015434+le-ander@users.noreply.github.com>
2023-02-03 14:54:16 +0100

    finalize dataloader

--- sfaira/data/dataloaders/loaders/d10_1038_s41586_021_03343_3/x_cerebralcortex_2021_smartseq2_agoglia_001.py
+++ sfaira/data/dataloaders/loaders/d10_1038_s41586_021_03343_3/x_cerebralcortex_2021_smartseq2_agoglia_001.py
@@ -2,9 +2,9 @@ import anndata
 import os
 import scipy.sparse
 import pandas as pd
+import numpy as np
 
 
-# the data_dir argument will be automatically set by sfaira to the folder where your datafiles lie
 def load(data_dir, **kwargs):
     fn = os.path.join(data_dir, "GSE144825_single_CS_GRCh38.txt.gz")
     fn_meta = os.path.join(data_dir, "GSE144825_single_CS_meta.txt.gz")
@@ -12,17 +12,19 @@ def load(data_dir, **kwargs):
     meta = pd.read_csv(fn_meta, sep="\t", index_col=0)
     meta.index.name = None
     meta["Plate"] = meta.index.str.split("_").str[0]
+    meta["organoid_age_days"] = "150"
+    meta["organoid_age_days"] = meta["organoid_age_days"].astype("category")
     meta["Plate"] = meta["Plate"].astype("category")
     meta["Line"] = meta["Line"].astype("category")
 
     adata = anndata.AnnData(
-        X=scipy.sparse.csr_matrix(data[data.index.str.endswith("human")].values.copy()),
+        X=scipy.sparse.csr_matrix(data[data.index.str.endswith("human")].values.copy(), dtype=np.float32),
         var=pd.DataFrame(index=data.columns),
         obs=meta,
         layers={
-            "human_counts": scipy.sparse.csr_matrix(data[data.index.str.endswith("human")].values.copy()),
-            "chimp_counts": scipy.sparse.csr_matrix(data[data.index.str.endswith("chimp")].values.copy()),
-            "total_counts": scipy.sparse.csr_matrix(data[data.index.str.endswith("total")].values.copy()),
+            "human_counts": scipy.sparse.csr_matrix(data[data.index.str.endswith("human")].values.copy(), dtype=np.float32),
+            "chimp_counts": scipy.sparse.csr_matrix(data[data.index.str.endswith("chimp")].values.copy(), dtype=np.float32),
+            "total_counts": scipy.sparse.csr_matrix(data[data.index.str.endswith("total")].values.copy(), dtype=np.float32),
         }
     )
 
