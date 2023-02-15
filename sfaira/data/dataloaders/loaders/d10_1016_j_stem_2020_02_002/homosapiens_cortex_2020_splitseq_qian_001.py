cad9cc8a49da55fd22211916c09feb649b5ebf0b
le-ander <20015434+le-ander@users.noreply.github.com>
2023-02-15 16:23:35 +0100

    review loader

--- sfaira/data/dataloaders/loaders/d10_1016_j_stem_2020_02_002/homosapiens_cortex_2020_splitseq_qian_001.py
+++ sfaira/data/dataloaders/loaders/d10_1016_j_stem_2020_02_002/homosapiens_cortex_2020_splitseq_qian_001.py
@@ -3,18 +3,12 @@ import os
 import tarfile
 import pandas as pd
 import numpy as np
-
+import scipy.sparse
 
 def load(data_dir, **kwargs):
-    dataset_tar_path = os.path.join(data_dir, "GSE137941_RAW.tar")
-    with tarfile.open(dataset_tar_path, "r") as tar:
-        tar.extractall(path=data_dir)
-    expression_path = os.path.join(data_dir, "GSM4094681_C1_150d_fseq12BC77_S2.deg.txt.gz")
-    expression_df = pd.read_csv(expression_path,
-                                compression='gzip', sep="\t", index_col=0, low_memory=False, header=0)
-    adata = anndata.AnnData(X=expression_df.T,
-                            obs=pd.DataFrame(expression_df.columns).set_index(0),
-                            var=pd.DataFrame(expression_df.index).set_index("GENE"), dtype=np.int64)
-    adata.obs.index.name = "cell"
-
+    with tarfile.open(os.path.join(data_dir, "GSE137941_RAW.tar"), "r") as tar:
+        df = pd.read_csv(tar.extractfile(tar.getmembers()[0]), compression='gzip', sep="\t", index_col=0, header=0)
+    adata = anndata.AnnData(X=scipy.sparse.csr_matrix(df.T.values, dtype=np.float32),
+                            obs=pd.DataFrame(index=df.columns.tolist()),
+                            var=pd.DataFrame(index=df.index.tolist()))
     return adata
