28a0f64a47f1d08e034c2794e588d324ff442975
ICB Artur Szalata <artur.szalata@helmholtz-muenchen.de>
2023-02-15 05:24:17 +0100

    fix lint

--- sfaira/data/dataloaders/loaders/d10_1016_j_stem_2020_02_002/homosapiens_cortex_2020_splitseq_qian_001.py
+++ sfaira/data/dataloaders/loaders/d10_1016_j_stem_2020_02_002/homosapiens_cortex_2020_splitseq_qian_001.py
@@ -11,8 +11,11 @@ def load(data_dir, **kwargs):
     with tarfile.open(dataset_tar_path, "r") as tar:
         tar.extractall(path=data_dir)
     expression_path = os.path.join(data_dir, "GSM4094681_C1_150d_fseq12BC77_S2.deg.txt.gz")
-    expression_df = pd.read_csv(expression_path, compression='gzip', sep="\t", index_col=0, low_memory=False, header=0)
-    adata = anndata.AnnData(X=expression_df.T, obs=pd.DataFrame(expression_df.columns).set_index(0), var=pd.DataFrame(expression_df.index).set_index("GENE"), dtype=np.int64)
+    expression_df = pd.read_csv(expression_path,
+                                compression='gzip', sep="\t", index_col=0, low_memory=False, header=0)
+    adata = anndata.AnnData(X=expression_df.T,
+                            obs=pd.DataFrame(expression_df.columns).set_index(0),
+                            var=pd.DataFrame(expression_df.index).set_index("GENE"), dtype=np.int64)
     adata.obs.index.name = "cell"
 
     return adata
