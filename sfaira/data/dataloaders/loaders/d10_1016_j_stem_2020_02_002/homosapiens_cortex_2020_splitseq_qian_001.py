c30b5fcae1b0939529d1a9b8d9215802952ccf85
ICB Artur Szalata <artur.szalata@helmholtz-muenchen.de>
2023-02-15 05:21:48 +0100

    add dataloader

---
+++ sfaira/data/dataloaders/loaders/d10_1016_j_stem_2020_02_002/homosapiens_cortex_2020_splitseq_qian_001.py
@@ -0,0 +1,18 @@
+import anndata
+import os
+import tarfile
+import pandas as pd
+import numpy as np
+
+
+
+def load(data_dir, **kwargs):
+    dataset_tar_path = os.path.join(data_dir, "GSE137941_RAW.tar")
+    with tarfile.open(dataset_tar_path, "r") as tar:
+        tar.extractall(path=data_dir)
+    expression_path = os.path.join(data_dir, "GSM4094681_C1_150d_fseq12BC77_S2.deg.txt.gz")
+    expression_df = pd.read_csv(expression_path, compression='gzip', sep="\t", index_col=0, low_memory=False, header=0)
+    adata = anndata.AnnData(X=expression_df.T, obs=pd.DataFrame(expression_df.columns).set_index(0), var=pd.DataFrame(expression_df.index).set_index("GENE"), dtype=np.int64)
+    adata.obs.index.name = "cell"
+
+    return adata
