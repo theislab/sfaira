a541dbb9163e7b5313cc8ba4d3b90775dae15257
le-ander <20015434+le-ander@users.noreply.github.com>
2023-02-16 19:03:07 +0100

    fix velasco loader

--- sfaira/data/dataloaders/loaders/d10_1038_s41586_019_1289_x/homosapiens_brain_2019_10x3v2_velasco_001.py
+++ sfaira/data/dataloaders/loaders/d10_1038_s41586_019_1289_x/homosapiens_brain_2019_10x3v2_velasco_001.py
@@ -13,7 +13,7 @@ def load(data_dir, sample_fn, **kwargs):
     expr_df = expr_df.apply(np.expm1)
     expr_df = expr_df.T
 
-    metadata_df = pd.read_csv(fn_meta, sep="\t", index_col=0, header=0)
+    metadata_df = pd.read_csv(fn_meta, sep="\t", index_col=0, header=0, low_memory=False)
     metadata_df = metadata_df.tail(-1)
 
     # add additional field for organoid age
@@ -21,13 +21,11 @@ def load(data_dir, sample_fn, **kwargs):
 
     # multiply by the UMIs, divide by 10^6
     arr = scipy.sparse.csr_matrix(
-        np.multiply(expr_df.values, metadata_df.nUMI.loc[
-            expr_df.index.tolist()].values.reshape(-1, 1).astype(int)))
+        np.multiply(expr_df.values, metadata_df.nUMI.loc[expr_df.index.tolist()].values.reshape(-1, 1).astype(int)), dtype=np.float32)
     arr /= 10**6
 
     adata = ad.AnnData(
-        X=arr, obs=metadata_df.loc[
-            expr_df.index.tolist()].copy(),
+        X=arr, obs=metadata_df.loc[expr_df.index.tolist()].copy(),
         var=pd.DataFrame(index=expr_df.columns.values))
 
     return adata
