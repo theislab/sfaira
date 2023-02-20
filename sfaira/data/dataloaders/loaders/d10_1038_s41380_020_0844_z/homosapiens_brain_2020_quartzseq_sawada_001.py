358e15d8dd33b78a92209c8bb72ca48ff93c67aa
le-ander <20015434+le-ander@users.noreply.github.com>
2023-02-20 12:40:37 +0100

    review loader

--- sfaira/data/dataloaders/loaders/d10_1038_s41380_020_0844_z/homosapiens_brain_2020_quartzseq_sawada_001.py
+++ sfaira/data/dataloaders/loaders/d10_1038_s41380_020_0844_z/homosapiens_brain_2020_quartzseq_sawada_001.py
@@ -4,32 +4,15 @@ import tarfile
 import anndata as ad
 import scipy.sparse
 import os
-import scanpy as sc
 
 
-def load(data_dir, **kwargs):
+def load(data_dir, sample_fn, **kwargs):
     fn = os.path.join(data_dir, "GSE120190_RAW.tar")
-    adatas = []
-    disease = []
-    sample = []
-
     with tarfile.open(fn) as tar:
-        for member in tar.getmembers():
-            d = pd.read_csv(tar.extractfile(member), compression="gzip", delimiter='\t', header=0, index_col=0)
-            d = d.T
-            for i in range(d.shape[0]):
-                disease.append(member.name.split("_")[2])
-                sample.append(member.name.split("_")[2] + "_" + member.name.split("_")[3])
-            temp = ad.AnnData(X=scipy.sparse.csr_matrix(d),
-                              var=pd.DataFrame(index=d.columns.values),
-                              obs=pd.DataFrame(index=d.index))
-            adatas.append(temp)
-
-    adata = ad.concat(adatas, join='outer')
-    adata.obs['disease'] = disease
-    adata.obs['person'] = "DT1_A"
-    adata.obs.loc[adata.obs['disease'] == "Unaffect", 'person'] = "DT1_U"
-    adata.obs['sample'] = sample
-    adata.obs['organoid_age_data'] = "35"
+        d = pd.read_csv(tar.extractfile(f"{sample_fn}_63mer_seqlev_d2_dge.txt.gz"), compression="gzip", delimiter='\t', header=0, index_col=0).T
+        adata = ad.AnnData(X=scipy.sparse.csr_matrix(d).astype(np.float32),
+                           var=pd.DataFrame(index=d.columns.values),
+                           obs=pd.DataFrame(index=d.index))
+    adata.obs['organoid_age_days'] = "35"
 
     return adata
