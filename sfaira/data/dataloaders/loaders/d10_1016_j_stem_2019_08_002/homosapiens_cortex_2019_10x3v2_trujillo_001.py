1a93ff874767989fa9d47681a50554b0f8107565
le-ander <20015434+le-ander@users.noreply.github.com>
2023-02-20 10:29:11 +0100

    review loader

--- sfaira/data/dataloaders/loaders/d10_1016_j_stem_2019_08_002/homosapiens_cortex_2019_10x3v2_trujillo_001.py
+++ sfaira/data/dataloaders/loaders/d10_1016_j_stem_2019_08_002/homosapiens_cortex_2019_10x3v2_trujillo_001.py
@@ -2,54 +2,26 @@ import numpy as np
 import pandas as pd
 import tarfile
 import anndata as ad
-import scipy.sparse
+import scipy.io
 import os
-import scanpy as sc
-import re
-from tempfile import TemporaryDirectory
-import shutil
 import gzip
 
 
-def load(data_dir, **kwargs):
+def load(data_dir, sample_fn, **kwargs):
+    age_dict = {
+        "1M": "30",
+        "3M": "90",
+        "6M": "180",
+        "10M": "300",
+    }
     fn = os.path.join(data_dir, "GSE130238_RAW.tar")
-    barcodes = []
-    genes = []
-    matrix = []
-
     with tarfile.open(fn) as tar:
-        for member in tar.getmembers():
-            temp = member.name
-            if re.search('barcodes', temp):
-                bar = pd.read_csv(tar.extractfile(member), delimiter='\t', compression="gzip", header=None, index_col=0)
-                bar.index.name = None
-                bar['time'] = member.name.split("_")[1]
-                if member.name.split("_")[1] == "1M":
-                    bar['organoid_age_days'] = "30"
-                elif member.name.split("_")[1] == "3M":
-                    bar['organoid_age_days'] = "90"
-                elif member.name.split("_")[1] == "6M":
-                    bar['organoid_age_days'] = "180"
-                else:
-                    bar['organoid_age_days'] = "300"
-                barcodes.append(bar)
-            elif re.search('genes', temp):
-                gen = pd.read_csv(tar.extractfile(member), delimiter='\t', compression='gzip', index_col=0, header=None, names=["ensembl_gene_id", "gene_id"])
-                genes.append(gen)
-            else:
-                fn_matrix = tar.extractfile(member)
-                uncompressed_file_type = "mtx"
-                with TemporaryDirectory() as tmpdir:
-                    tmppth = tmpdir + f"/decompressed.{uncompressed_file_type}"
-                    with gzip.open(fn_matrix, "rb") as input_f, open(tmppth, "wb") as output_f:
-                        shutil.copyfileobj(input_f, output_f)
-                    mat = scipy.io.mmread(tmppth)
-                matrix.append(mat.T)
-
-    adatas = []
-    for i in range(len(matrix)):
-        temp = ad.AnnData(X=matrix[i], obs=barcodes[i], var=genes[i])
-        adatas.append(temp)
-    adata = ad.concat(adatas, merge='first')
-
+        with gzip.open(tar.extractfile(f"{sample_fn}_cortical_organoids_matrix.mtx.gz"), "rb") as mm:
+            x = scipy.io.mmread(mm).T.tocsr().astype(np.float32)
+        obs = pd.read_csv(tar.extractfile(f"{sample_fn}_cortical_organoids_barcodes.tsv.gz"),
+                          delimiter='\t', compression="gzip", header=None, index_col=0, names=[None])
+        obs['organoid_age_days'] = age_dict[sample_fn.split("_")[1]]
+        var = pd.read_csv(tar.extractfile(f"{sample_fn}_cortical_organoids_genes.tsv.gz"),
+                          delimiter='\t', compression='gzip', index_col=0, header=None, names=[None, "gene_id"])
+    adata = ad.AnnData(X=x, obs=obs, var=var)
     return adata
