0cd4547c37dc3845eaeacbd61a22e64a4b9a6721
le-ander <20015434+le-ander@users.noreply.github.com>
2023-02-22 19:23:46 +0100

    review dataloader

--- sfaira/data/dataloaders/loaders/d10_1038_s41591_020_1043_9/homosapiens_x_2020_10x3v2_khan_001.py
+++ sfaira/data/dataloaders/loaders/d10_1038_s41591_020_1043_9/homosapiens_x_2020_10x3v2_khan_001.py
@@ -1,78 +1,23 @@
+import os
 import numpy as np
 import pandas as pd
+import gzip
+import scipy
 import tarfile
 import anndata as ad
-import scipy.sparse
-import os
-import scanpy as sc
-import re
-from tempfile import TemporaryDirectory
-import shutil
-import gzip
+import scipy.io
 
 
-def load(data_dir, **kwargs):
+def load(data_dir, sample_fn, **kwargs):
     fn = os.path.join(data_dir, "GSE145122_RAW.tar")
-    barcodes = []
-    genes = []
-    matrix = []
     with tarfile.open(fn) as tar:
-        for member in tar.getmembers():
-            temp = member.name
-            if re.search('barcodes', temp):
-                bar = pd.read_csv(tar.extractfile(member), delimiter='\t', compression="gzip", header=None, index_col=0)
-                bar.index.name = None
-                sample = member.name.split("_")[1] + "_" + member.name.split("_")[2]
-                bar['sample'] = sample
-                if sample == "Control_1":
-                    bar['cell_line'] = "0524-1"
-                    bar['sex'] = "female"
-                    bar['disease'] = "none"
-                    bar['diagnosis'] = "none"
-                    bar['full_scale_IQ'] = "120"
-                    bar['subject_age'] = "27 ys"
-                elif sample == "Control_2":
-                    bar['cell_line'] = "0307-1"
-                    bar['sex'] = "male"
-                    bar['disease'] = "none"
-                    bar['diagnosis'] = "none"
-                    bar['full_scale_IQ'] = "N/A"
-                    bar['subject_age'] = "27 ys"
-                elif sample == "Patient_1":
-                    bar['cell_line'] = "7958-3"
-                    bar['sex'] = "female"
-                    bar['disease'] = "22q11DS"
-                    bar['diagnosis'] = "Schizophrenia, Depressive Disorder Not Otherwise Specified"
-                    bar['full_scale_IQ'] = "56"
-                    bar['subject_age'] = "25 ys"
-                else:
-                    bar['cell_line'] = "6303-5"
-                    bar['sex'] = "female"
-                    bar['disease'] = "22q11DS"
-                    bar['diagnosis'] = "no psychiatric diagnoses "
-                    bar['full_scale_IQ'] = "65"
-                    bar['subject_age'] = "22 ys"
-                barcodes.append(bar)
-            elif re.search('features', temp):
-                gen = pd.read_csv(tar.extractfile(member), delimiter='\t', compression='gzip',
-                                  index_col=0, header=None, names=["ensembl_gene_id", "gene_id", "status"])
-                genes.append(gen)
-            else:
-                fn_matrix = tar.extractfile(member)
-                uncompressed_file_type = "mtx"
-                with TemporaryDirectory() as tmpdir:
-                    tmppth = tmpdir + f"/decompressed.{uncompressed_file_type}"
-                    with gzip.open(fn_matrix, "rb") as input_f, open(tmppth, "wb") as output_f:
-                        shutil.copyfileobj(input_f, output_f)
-                    mat = scipy.io.mmread(tmppth)
-                matrix.append(mat.T)
-
-    adatas = []
-    for i in range(len(matrix)):
-        temp = ad.AnnData(X=matrix[i], obs=barcodes[i], var=genes[i])
-        adatas.append(temp)
-    adata = ad.concat(adatas, merge='first')
-    adata.obs['organoid_age_days'] = "83"
-    del (adata.var['status'])
-
+        obs = pd.read_csv(tar.extractfile(f"{sample_fn}_barcodes.tsv.gz"),
+                          compression="gzip", header=None, index_col=0, names=[None])
+        var = pd.read_csv(tar.extractfile(f"{sample_fn}_features.tsv.gz"),
+                          compression="gzip", sep='\t', header=None, usecols=[0, 1], index_col=1, names=["ensembl_id", None])
+        with gzip.open(tar.extractfile(f"{sample_fn}_matrix.mtx.gz"), 'rb') as mm:
+            x = scipy.io.mmread(mm).T.tocsr().astype(np.float32)
+    obs["organoid_age_days"] = "83"
+    adata = ad.AnnData(X=x, obs=obs, var=var)
+    adata.var_names_make_unique()
     return adata
