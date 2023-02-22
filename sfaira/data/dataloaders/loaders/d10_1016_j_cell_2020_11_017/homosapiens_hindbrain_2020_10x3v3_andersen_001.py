fe912d89b9492a258725dd06a92348da1db81539
le-ander <20015434+le-ander@users.noreply.github.com>
2023-02-22 15:15:38 +0100

    review dataloader

--- sfaira/data/dataloaders/loaders/d10_1016_j_cell_2020_11_017/homosapiens_hindbrain_2020_10x3v3_andersen_001.py
+++ sfaira/data/dataloaders/loaders/d10_1016_j_cell_2020_11_017/homosapiens_hindbrain_2020_10x3v3_andersen_001.py
@@ -1,31 +1,32 @@
 import tarfile
 import os
-
-
 import pandas as pd
 import numpy as np
 import anndata as ad
-from scipy.io import mmread
+import scipy.io
+import scipy.sparse
+import gzip
 
 
-def load(data_dir, **kwargs):
-    dataset_tar_path = os.path.join(data_dir, "GSE123722_RAW.tar")
-    with tarfile.open(dataset_tar_path, "r") as tar:
-        tar.extractall(data_dir)
-
-    matrix_path = os.path.join(data_dir, "GSM4589792_hSpS_d53_matrix.mtx.gz")
-    genes_path = os.path.join(data_dir, "GSM4589792_hSpS_d53_features.tsv.gz")
-    barcodes_path = os.path.join(data_dir, "GSM4589792_hSpS_d53_barcodes.tsv.gz")
-
-    genes_df = pd.read_csv(genes_path, compression='gzip', sep="\t", header=None)
-    genes_df = genes_df.drop(columns=2)
-    genes_df.columns = ["Ensembl", "Gene_Symbol"]
-
-    cells_df = pd.read_csv(barcodes_path, compression='gzip', sep="\t", header=None)
-    cells_df.columns = ["Barcode"]
-
-    expression_mt = mmread(matrix_path)
-
-    adata = ad.AnnData(X=expression_mt.T, obs=cells_df, var=genes_df, dtype=np.int64)
-
+def load(data_dir, sample_fn, **kwargs):
+    fn = os.path.join(data_dir, "GSE123722_RAW.tar")
+    with tarfile.open(fn, "r") as tar:
+        if sample_fn == "GSM4589792_hSpS_d53":
+            obs = pd.read_csv(tar.extractfile(f"{sample_fn}_barcodes.tsv.gz"),
+                              compression="gzip", header=None, index_col=0, names=[None])
+            var = pd.read_csv(tar.extractfile(f"{sample_fn}_features.tsv.gz"),
+                              compression="gzip", sep='\t', header=None, usecols=[0, 1], index_col=1, names=["ensembl_id", None])
+            with gzip.open(tar.extractfile(f"{sample_fn}_matrix.mtx.gz"), 'rb') as mm:
+                x = scipy.io.mmread(mm).T.tocsr().astype(np.float32)
+            adata = ad.AnnData(X=x, obs=obs, var=var)
+            adata.var_names_make_unique()
+            adata.obs["cellline"] = adata.obs.index.str.split("-").str[1]
+            adata.obs["cellline"].replace({"1": "8858-1", "2": "1205-4", "3": "0524-1"}, inplace=True)
+        else:
+            df = pd.read_csv(tar.extractfile(f"{sample_fn}_MolsPerCell.csv.gz"), compression="gzip", index_col=0)
+            adata = ad.AnnData(X=scipy.sparse.csr_matrix(df, dtype=np.float32),
+                               var=pd.DataFrame(index=df.columns),
+                               obs=pd.DataFrame(index=df.index.tolist()))
+            adata.obs["cellline"] = "8858-1"
+    adata.obs["organoid_age_days"] = sample_fn.split("_")[2][1:]
     return adata
