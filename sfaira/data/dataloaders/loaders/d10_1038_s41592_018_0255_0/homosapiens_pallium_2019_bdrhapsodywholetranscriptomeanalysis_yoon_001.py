f5e0c3966e6085bb45cd3338950e041840141d24
le-ander <20015434+le-ander@users.noreply.github.com>
2023-02-23 11:58:45 +0100

    reviewing dataloader

--- sfaira/data/dataloaders/loaders/d10_1038_s41592_018_0255_0/homosapiens_pallium_2019_bdrhapsodywholetranscriptomeanalysis_yoon_001.py
+++ sfaira/data/dataloaders/loaders/d10_1038_s41592_018_0255_0/homosapiens_pallium_2019_bdrhapsodywholetranscriptomeanalysis_yoon_001.py
@@ -4,25 +4,15 @@ import tarfile
 import anndata as ad
 import scipy.sparse
 import os
-import scanpy as sc
 
 
-def load(data_dir, **kwargs):
+def load(data_dir, sample_fn, **kwargs):
     fn = os.path.join(data_dir, "GSE107771_RAW.tar")
-    dfs = []
-    obs = []
-    with tarfile.open(fn) as tar:
-        for member in tar.getmembers():
-            d = pd.read_csv(tar.extractfile(member), compression="gzip", header=0, index_col=0)
-            for i in range(d.shape[0]):
-                temp = member.name.split("_")[1] + '_' + member.name.split("_")[2] + member.name.split("_")[3]
-                obs.append(temp)
-            dfs.append(d)
-
-    adata = ad.AnnData(
-        X=scipy.sparse.csr_matrix(np.vstack((dfs[0], dfs[1], dfs[2]))),
-        obs=pd.DataFrame({"sample": obs}),
-        var=pd.DataFrame(index=dfs[0].columns.values))
-    adata.obs['organoid_age_days'] = np.repeat(105, len(adata.obs))
+    with tarfile.open(fn, "r") as tar:
+        df = pd.read_csv(tar.extractfile(f"{sample_fn}_MolsPerCell.csv.gz"), compression="gzip", index_col=0)
+        adata = ad.AnnData(X=scipy.sparse.csr_matrix(df, dtype=np.float32),
+                           var=pd.DataFrame(index=df.columns),
+                           obs=pd.DataFrame(index=df.index.tolist()))
+    adata.obs['organoid_age_days'] = "105"
 
     return adata
