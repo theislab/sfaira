79a0d06c31714f8a0c91b99aba0edbb5347c197a
ICB Irena Sliskovic <irena.sliskovic@cpusrv42.scidom.de>
2023-02-28 15:04:05 +0000

    fix flake8

--- sfaira/data/dataloaders/loaders/d10_1038_s41586_020_1962_0/homosapiens_forebrain_2020_10x3v2_bhaduri_001.py
+++ sfaira/data/dataloaders/loaders/d10_1038_s41586_020_1962_0/homosapiens_forebrain_2020_10x3v2_bhaduri_001.py
@@ -23,7 +23,7 @@ def load(data_dir, **kwargs):
         "H28126SWeek5": ["H28126", "35", "Least Direct [L]"],
         "H28126XWeek5": ["H28126", "35", "Most Directed [M]"],
         "H28126PWeek5": ["H28126", "35", "Directed [D]"],
-        "H28126S2Week5": ["H28126", "35", "Least Direct [L]"],  #hES-derived cerebral organoid
+        "H28126S2Week5": ["H28126", "35", "Least Direct [L]"],  # hES-derived cerebral organoid
         "H28126SWeek8": ["H28126", "56", "Least Direct [L]"],
         "H28126XWeek8": ["H28126", "56", "Most Directed [M]"],
         "H28126S2Week8": ["H28126", "56", "Least Direct [L]"],
@@ -43,9 +43,9 @@ def load(data_dir, **kwargs):
         "Week8P_1323_4": ["1323_4", "56", "Directed [D]"],
         "Week10S_1323_4": ["1323_4", "70", "Least Direct [L]"],
         "Week10P_1323_4": ["1323_4", "70", "Directed [D]"],
-        "L13234PWeek24": ["1323_4", "168", "Directed [D]"],  #GEO wk2413234P, female; I think this is 1323_4
+        "L13234PWeek24": ["1323_4", "168", "Directed [D]"],  # GEO wk2413234P, female; I think this is 1323_4
         "L13234SWeek24": ["1323_4", "168", "Least Direct [L]"],  # GEO wk2413234S, female; I think this is 1323_4
-        "L13234SWeek15": ["1323_4", "105", "Least Direct [L]"],  # male, 10x v2, in GEO says its age is 24 weeks, but the name suggests 15 weeks?    
+        "L13234SWeek15": ["1323_4", "105", "Least Direct [L]"]  # male, 10x v2, in GEO says its age is 24 weeks, but the name suggests 15 weeks?    
     }
     fn = os.path.join(data_dir, "GSE132672_allorganoids_withnew_matrix.txt.gz")
     df = pd.read_csv(fn, delimiter='\t', compression='gzip', index_col=0)
