283e7dd883ad8a843856c17fc521cdb701d234b3
le-ander <20015434+le-ander@users.noreply.github.com>
2023-03-01 15:02:17 +0100

    update fleck loader with public data

--- sfaira/data/dataloaders/loaders/d10_1038_s41586_022_05279_8/homosapiens_brain_2022_10x3v3_fleck_001.py
+++ sfaira/data/dataloaders/loaders/d10_1038_s41586_022_05279_8/homosapiens_brain_2022_10x3v3_fleck_001.py
@@ -3,14 +3,19 @@ import os
 import scipy.io
 import pandas as pd
 import numpy as np
+import tarfile
+import gzip
 
 
 def load(data_dir, **kwargs):
-    x = scipy.io.mmread(os.path.join(data_dir, 'counts.mtx.gz')).T.tocsr().astype(np.float32)
-    var = pd.read_csv(os.path.join(data_dir, 'features.tsv.gz'), sep="\t", header=None, index_col=0, names=[None])
-    obs = pd.read_csv(os.path.join(data_dir, 'barcodes.tsv.gz'), sep="\t", index_col=0, header=None, names=[None])
+    fn = os.path.join(data_dir, "rna_matrices.tar.gz")
+    with tarfile.open(fn) as tar:
+        with gzip.open(tar.extractfile("data_matrices/counts.mtx.gz"), "rb") as mm:
+            x = scipy.io.mmread(mm).T.tocsr().astype(np.float32)
+        var = pd.read_csv(tar.extractfile("data_matrices/features.tsv.gz"), compression="gzip", sep="\t", header=None, index_col=0, names=[None])
+        obs = pd.read_csv(tar.extractfile("data_matrices/barcodes.tsv.gz"), compression="gzip", sep="\t", index_col=0, header=None, names=[None])
+        meta = pd.read_csv(tar.extractfile("data_matrices/meta.tsv.gz"), compression="gzip", sep="\t", index_col=0)
 
-    meta = pd.read_csv(os.path.join(data_dir, 'meta.tsv.gz'), sep="\t", index_col=0)
     meta = meta.loc[obs.index]
     meta["organoid_age_days"] = [i.split("_")[1] for i in meta["sample"]]
     meta["celltype"] = meta["lineage"] + "-" + meta["stage_manual"]
