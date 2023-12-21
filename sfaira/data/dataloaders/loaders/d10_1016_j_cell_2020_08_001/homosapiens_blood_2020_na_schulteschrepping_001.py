5fcf932c44613e6bf4063ef3075ad6062bf5d9a1
le-ander <20015434+le-ander@users.noreply.github.com>
2023-12-21 09:20:42 +0100

    round restored counts in some affected datasets

--- sfaira/data/dataloaders/loaders/d10_1016_j_cell_2020_08_001/homosapiens_blood_2020_na_schulteschrepping_001.py
+++ sfaira/data/dataloaders/loaders/d10_1016_j_cell_2020_08_001/homosapiens_blood_2020_na_schulteschrepping_001.py
@@ -1,11 +1,13 @@
 import anndata
 import os
 import scipy.sparse
+import numpy as np
 
 
 def load(data_dir, sample_fn, **kwargs):
     fn = os.path.join(data_dir, sample_fn)
     adata = anndata.read_h5ad(fn)
+    adata.X = np.round((np.expm1(adata.X.A) * adata.obs["nCount_RNA"][:, None] / 10000))
     adata.X = scipy.sparse.csr_matrix(adata.X)
 
     return adata
--- sfaira/data/dataloaders/loaders/d10_1038_s41422_018_0099_2/homosapiens_testis_2018_10x3v2_guo_001.py
+++ sfaira/data/dataloaders/loaders/d10_1038_s41422_018_0099_2/homosapiens_testis_2018_10x3v2_guo_001.py
@@ -9,5 +9,6 @@ def load(data_dir, **kwargs):
     adata = anndata.read_h5ad(fn)
     adata.X = np.expm1(adata.X)
     adata.X = adata.X.multiply(scipy.sparse.csc_matrix(adata.obs["n_counts"].values[:, None])).multiply(1 / 10000)
+    adata.X + np.round(adata.X)
 
     return adata
